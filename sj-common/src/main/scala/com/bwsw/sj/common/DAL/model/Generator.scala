c33956f8659b05d880b2bfc85674a830bdabee3a
Mendelbaum Nikita <mendelbaum_nm@bw-sw.com>
2016-05-04 14:28:26 +0600

    SJ-1817 Generator class fix

--- sj-common/src/main/scala/com/bwsw/sj/common/DAL/model/Generator.scala
+++ sj-common/src/main/scala/com/bwsw/sj/common/DAL/model/Generator.scala
@@ -1,5 +1,7 @@
 package com.bwsw.sj.common.DAL.model
 
+import org.mongodb.morphia.annotations.Reference
+
 class Generator() {
   var generatorType: String = null
   @Reference var service: Service = null
