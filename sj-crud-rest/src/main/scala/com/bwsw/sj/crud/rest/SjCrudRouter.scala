6682f4006c9cf8e3a4166e24a761fcc899a9b8ec
Kseniya Tomskikh <tomskikh_ka@bitworks-software.com>
2016-05-19 18:34:27 +0600

    SJ-1927 Change format of response

--- sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/SjCrudRouter.scala
+++ sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/SjCrudRouter.scala
@@ -8,7 +8,7 @@ import akka.http.scaladsl.model.{EntityStreamSizeException, HttpEntity, HttpResp
 import akka.http.scaladsl.server.{Directives, ExceptionHandler}
 import com.bwsw.common.exceptions.{BadRecord, BadRecordWithKey, InstanceException, KeyAlreadyExists}
 import com.bwsw.sj.crud.rest.api.{SjCustomApi, SjModulesApi, SjServiceApi, SjStreamsApi}
-import com.bwsw.sj.crud.rest.entities.Response
+import com.bwsw.sj.crud.rest.entities.ProtocolResponse
 import org.everit.json.schema.ValidationException
 
 /**
@@ -27,40 +27,40 @@ trait SjCrudRouter extends Directives
     case BadRecord(msg) =>
       complete(HttpResponse(
         BadRequest,
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(400, null, msg)))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(400, Map("message" -> msg))))
       ))
     case BadRecordWithKey(msg, key) =>
       complete(HttpResponse(
         BadRequest,
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(400, key, msg)))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(400, Map("message" -> msg))))
       ))
     case InstanceException(msg, key) =>
       complete(HttpResponse(
         BadRequest,
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(400, key, msg)))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(400, Map("message" -> msg))))
       ))
     case KeyAlreadyExists(msg, key) =>
       complete(HttpResponse(
         BadRequest,
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(400, key, msg)))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(400, Map("message" -> msg))))
       ))
     case ex: ValidationException =>
       complete(HttpResponse(
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(500, null, "Specification.json is invalid")))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(500, Map("message" -> "Specification.json is invalid"))))
       ))
     case ex: EntityStreamSizeException =>
       complete(HttpResponse(
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(500, null, "File is very large")))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(500, Map("message" -> "File is very large"))))
       ))
     case ex: FileNotFoundException =>
       complete(HttpResponse(
         BadRequest,
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(400, null, ex.getMessage)))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(400, Map("message" -> ex.getMessage))))
       ))
     case ex: Exception =>
       complete(HttpResponse(
         InternalServerError,
-        entity = HttpEntity(`application/json`, serializer.serialize(Response(500, null, "Internal server error: " + ex.getMessage)))
+        entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(500, Map("message" -> s"Internal server error: ${ex.getMessage}"))))
       ))
   }
 
