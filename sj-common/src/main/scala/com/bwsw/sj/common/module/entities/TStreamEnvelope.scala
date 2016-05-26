7e6485635c934e0ebb6e21500e37274e89dee319
Kseniya Tomskikh <tomskikh_ka@bitworks-software.com>
2016-05-26 13:38:19 +0600

    SJ-1950 Deleted classes from sj.common

---
+++ sj-common/src/main/scala/com/bwsw/sj/common/module/entities/TStreamEnvelope.scala
@@ -0,0 +1,46 @@
+package com.bwsw.sj.common.module.entities
+
+import java.util.UUID
+
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type
+import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
+
+/**
+ * Represents a message envelope that is received by an Executor for each message
+ * that is received from a partition of a specific input kafka stream or t-stream.
+ */
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "streamType")
+@JsonSubTypes(Array(
+  new Type(value = classOf[TStreamEnvelope], name = "t-stream"),
+  new Type(value = classOf[KafkaEnvelope], name = "kafka-stream")
+))
+class Envelope() {
+  var streamType: String = null
+  var stream: String = null
+  var partition: Int = 0
+  var tags: Array[String] = null
+}
+
+/**
+ * Provides a wrapper for t-stream transaction.
+ * Created: 12/04/2016
+ * @author Kseniya Mikhaleva
+ */
+
+class TStreamEnvelope() extends Envelope() {
+  var txnUUID: UUID = null
+  var consumerName: String = null
+  var data: List[Array[Byte]] = null
+  streamType = "t-stream"
+}
+
+/**
+ * Provides a wrapper for kafka message.
+ */
+class KafkaEnvelope() extends Envelope() {
+  var data: Array[Byte] = null
+  var offset: Long = 0
+  streamType = "kafka-stream"
+
+}
---
+++ sj-common/src/main/scala/com/bwsw/sj/common/module/environment/PartitionedOutput.scala
@@ -0,0 +1,28 @@
+package com.bwsw.sj.common.module.environment
+
+import com.bwsw.tstreams.agents.producer.{BasicProducer, BasicProducerTransaction, ProducerPolicies}
+import org.slf4j.LoggerFactory
+import scala.collection._
+
+/**
+ * Provides an output stream that defined for each partition
+ * Created: 20/04/2016
+ * @author Kseniya Mikhaleva
+ *
+ * @param producer Producer for specific output of stream
+ */
+
+class PartitionedOutput(producer: BasicProducer[Array[Byte], Array[Byte]]) {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+  private val txns = mutable.Map[Int, BasicProducerTransaction[Array[Byte], Array[Byte]]]()
+
+  def put(data: Array[Byte], partition: Int) =
+    if (txns.contains(partition)) {
+      txns(partition).send(data)
+    }
+    else {
+      txns(partition) = producer.newTransaction(ProducerPolicies.errorIfOpen, partition)
+      txns(partition).send(data)
+    }
+}
