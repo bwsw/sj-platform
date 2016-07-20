e9ea807746355efbe99d07bdb1866ec69956d375
Kseniya Tomskikh <tomskikh_ka@bitworks-software.com>
2016-07-20 11:36:55 +0600

    SJ-2110 Added instance util object

--- core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/SjCrudRouter.scala
+++ core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/SjCrudRouter.scala
@@ -75,6 +75,7 @@ with SjConfigSettingsApi {
           messages.getString("rest.errors.unrecognized_property"), ex.getPropertyName, ex.getKnownPropertyIds)))))
       ))
     case ex: Exception =>
+      ex.printStackTrace()
       complete(HttpResponse(
         InternalServerError,
         entity = HttpEntity(`application/json`, serializer.serialize(ProtocolResponse(500, Map("message" -> MessageFormat.format(
