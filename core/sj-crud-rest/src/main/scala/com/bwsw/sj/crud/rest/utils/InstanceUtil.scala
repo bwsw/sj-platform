03bbbc1eb7fce62bb912f7eba8447b07d4178357
Kseniya Tomskikh <tomskikh_ka@bitworks-software.com>
2016-07-21 10:13:07 +0600

    SJ-2110 Added stopping not started (stopped) instance.

--- core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/utils/InstanceUtil.scala
+++ core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/utils/InstanceUtil.scala
@@ -4,6 +4,8 @@ import com.bwsw.sj.common.DAL.model.module.Instance
 import com.bwsw.sj.common.DAL.repository.ConnectionRepository
 import com.bwsw.sj.common.DAL.service.GenericMongoService
 import com.bwsw.sj.common.ModuleConstants._
+import com.bwsw.sj.crud.rest.runner.InstanceDestroyer
+import org.slf4j.LoggerFactory
 
 /**
   * Created: 20/07/2016
@@ -11,9 +13,11 @@ import com.bwsw.sj.common.ModuleConstants._
   * @author Kseniya Tomskikh
   */
 object InstanceUtil {
+  private val logger = LoggerFactory.getLogger(this.getClass.getName)
   private val instanceDAO: GenericMongoService[Instance] = ConnectionRepository.getInstanceService
 
   def checkStatusInstances() = {
+    logger.info("Run crud-rest. Check instances.")
     val instances = instanceDAO.getAll.filter { instance =>
       instance.status.equals(starting) ||
         instance.status.equals(stopping) ||
@@ -21,7 +25,7 @@ object InstanceUtil {
     }
 
     instances.foreach { instance =>
-
+      new Thread(new InstanceDestroyer(instance, 1000)).start()
     }
   }
 
