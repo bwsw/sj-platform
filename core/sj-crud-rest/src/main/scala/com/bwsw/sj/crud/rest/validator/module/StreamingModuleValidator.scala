2192fc8e29f45d71df4c844f223271ad706a70d1
mikhaljova_ka <buterffly@sibmail.com>
2016-08-10 09:58:10 +0700

    SJ-2212 is done. fix rest bug related with instance name with a long in 1 letter

--- core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/validator/module/StreamingModuleValidator.scala
+++ core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/validator/module/StreamingModuleValidator.scala
@@ -92,7 +92,7 @@ abstract class StreamingModuleValidator {
     logger.debug(s"Instance: ${parameters.name}. General options validation.")
     val errors = new ArrayBuffer[String]()
 
-    if (!parameters.name.matches("""^([a-zA-Z][a-zA-Z0-9-]+)$""")) {
+    if (!parameters.name.matches("""^([a-zA-Z][a-zA-Z0-9-]*)$""")) {
       errors += s"Instance has incorrect name: ${parameters.name}. Name of instance must be contain digits, letters or hyphens. First symbol must be letter."
     }
 
