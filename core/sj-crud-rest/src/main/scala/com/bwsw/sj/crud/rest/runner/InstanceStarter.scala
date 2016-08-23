bab6084038ff64dd24894c79f48ab78bcd8b0a7f
mikhaljova_ka <buterffly@sibmail.com>
2016-08-23 16:03:13 +0700

    SJ-2249 add two env variables: ZOOKEEPER_HOST and ZOOKEEPER_PORT, if they absent use mesos zk

--- core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/runner/InstanceStarter.scala
+++ core/sj-crud-rest/src/main/scala/com/bwsw/sj/crud/rest/runner/InstanceStarter.scala
@@ -17,13 +17,14 @@ import org.apache.http.util.EntityUtils
 import org.slf4j.LoggerFactory
 
 /**
-  * One-thread starting object for instance
-  * using synchronous apache http client
-  * Created: 13/05/2016
-  *
-  * @author Kseniya Tomskikh
-  */
+ * One-thread starting object for instance
+ * using synchronous apache http client
+ * Created: 13/05/2016
+ *
+ * @author Kseniya Tomskikh
+ */
 class InstanceStarter(instance: Instance, delay: Long) extends Runnable {
+
   import InstanceMethods._
   import com.bwsw.sj.common.ModuleConstants._
 
@@ -40,9 +41,7 @@ class InstanceStarter(instance: Instance, delay: Long) extends Runnable {
         val entity = serializer.deserialize[Map[String, Any]](EntityUtils.toString(mesosInfoResponse.getEntity, "UTF-8"))
         val mesosMaster = entity.get("marathon_config").get.asInstanceOf[Map[String, Any]].get("master").get.asInstanceOf[String]
 
-        val mesosMasterUrl = new URI(mesosMaster)
-        val zooKeeperServers = new util.ArrayList[InetSocketAddress]()
-        zooKeeperServers.add(new InetSocketAddress(mesosMasterUrl.getHost, mesosMasterUrl.getPort))
+        val zooKeeperServers = getZooKeeperServers(mesosMaster)
         val zkClient = new ZooKeeperClient(Amount.of(zkSessionTimeout, Time.MILLISECONDS), zooKeeperServers)
 
         var isMaster = false
@@ -89,9 +88,23 @@ class InstanceStarter(instance: Instance, delay: Long) extends Runnable {
     instanceDAO.save(instance)
   }
 
+  private def getZooKeeperServers(mesosMaster: String) = {
+    val zooKeeperServers = new util.ArrayList[InetSocketAddress]()
+    val restHost = System.getenv("ZOOKEEPER_HOST")
+    val restPort = System.getenv("ZOOKEEPER_PORT")
+    if (restHost != null && restPort != null) {
+      zooKeeperServers.add(new InetSocketAddress(restHost, restPort.toInt))
+    } else {
+      val mesosMasterUrl = new URI(mesosMaster)
+      zooKeeperServers.add(new InetSocketAddress(mesosMasterUrl.getHost, mesosMasterUrl.getPort))
+    }
+
+    zooKeeperServers
+  }
+
   /**
-    * Starting of instance if all generators is started
-    */
+   * Starting of instance if all generators is started
+   */
   def instanceStart(mesosMaster: String) = {
     logger.debug(s"Instance: ${instance.name}. Instance is starting.")
     stageUpdate(instance, instance.name, starting)
@@ -100,27 +113,27 @@ class InstanceStarter(instance: Instance, delay: Long) extends Runnable {
     startFrameworkResult match {
       case Right(response) =>
         if (response.getStatusLine.getStatusCode.equals(OK) ||
-        response.getStatusLine.getStatusCode.equals(Created)) {
+          response.getStatusLine.getStatusCode.equals(Created)) {
 
-        while (!isStarted) {
-          Thread.sleep(delay)
-          val taskInfoResponse = getTaskInfo(instance.name)
-          if (taskInfoResponse.getStatusLine.getStatusCode == OK) {
-            val entity = serializer.deserialize[Map[String, Any]](EntityUtils.toString(taskInfoResponse.getEntity, "UTF-8"))
-            val tasksRunning = entity("app").asInstanceOf[Map[String, Any]]("tasksRunning").asInstanceOf[Int]
-            if (tasksRunning == 1) {
-              instance.status = started
-              stageUpdate(instance, instance.name, started)
-              isStarted = true
+          while (!isStarted) {
+            Thread.sleep(delay)
+            val taskInfoResponse = getTaskInfo(instance.name)
+            if (taskInfoResponse.getStatusLine.getStatusCode == OK) {
+              val entity = serializer.deserialize[Map[String, Any]](EntityUtils.toString(taskInfoResponse.getEntity, "UTF-8"))
+              val tasksRunning = entity("app").asInstanceOf[Map[String, Any]]("tasksRunning").asInstanceOf[Int]
+              if (tasksRunning == 1) {
+                instance.status = started
+                stageUpdate(instance, instance.name, started)
+                isStarted = true
+              }
+            } else {
+              stageUpdate(instance, instance.name, starting)
             }
-          } else {
-            stageUpdate(instance, instance.name, starting)
           }
+        } else {
+          instance.status = failed
+          stageUpdate(instance, instance.name, failed)
         }
-      } else {
-        instance.status = failed
-        stageUpdate(instance, instance.name, failed)
-      }
       case Left(isRunning) =>
         if (!isRunning) {
           instance.status = failed
@@ -133,52 +146,52 @@ class InstanceStarter(instance: Instance, delay: Long) extends Runnable {
   }
 
   /**
-    * Running framework on mesos
-    *
-    * @return - Response from marathon or flag of running framework
-    *         (true, if framework running on mesos)
-    */
+   * Running framework on mesos
+   *
+   * @return - Response from marathon or flag of running framework
+   *         (true, if framework running on mesos)
+   */
   def frameworkStart(mesosMaster: String) = {
-      logger.debug(s"Instance: ${instance.name}. Start framework for instance.")
-      val frameworkJarName = ConfigUtils.getFrameworkJarName()
-      val restUrl = new URI(s"$restAddress/v1/custom/jars/$frameworkJarName")
-      val taskInfoResponse = getTaskInfo(instance.name)
-      if (taskInfoResponse.getStatusLine.getStatusCode.equals(OK)) {
-        val ignore = serializer.getIgnoreUnknown()
-        serializer.setIgnoreUnknown(true)
-        val entity = serializer.deserialize[MarathonRequest](EntityUtils.toString(taskInfoResponse.getEntity, "UTF-8"))
-        serializer.setIgnoreUnknown(ignore)
-        if (entity.instances < 1) {
-          Right(scaleApplication(instance.name, 1))
-        } else {
-          Left(true)
-        }
+    logger.debug(s"Instance: ${instance.name}. Start framework for instance.")
+    val frameworkJarName = ConfigUtils.getFrameworkJarName()
+    val restUrl = new URI(s"$restAddress/v1/custom/jars/$frameworkJarName")
+    val taskInfoResponse = getTaskInfo(instance.name)
+    if (taskInfoResponse.getStatusLine.getStatusCode.equals(OK)) {
+      val ignore = serializer.getIgnoreUnknown()
+      serializer.setIgnoreUnknown(true)
+      val entity = serializer.deserialize[MarathonRequest](EntityUtils.toString(taskInfoResponse.getEntity, "UTF-8"))
+      serializer.setIgnoreUnknown(ignore)
+      if (entity.instances < 1) {
+        Right(scaleApplication(instance.name, 1))
       } else {
-        var applicationEnvs = Map(
-          "MONGO_HOST" -> ConnectionConstants.mongoHost,
-          "MONGO_PORT" -> s"${ConnectionConstants.mongoPort}",
-          "INSTANCE_ID" -> instance.name,
-          "MESOS_MASTER" -> mesosMaster
-        )
-        if (instance.environmentVariables != null) {
-          applicationEnvs = applicationEnvs ++ Map(instance.environmentVariables.asScala.toList: _*)
-        }
-        val request = new MarathonRequest(instance.name,
-          "java -jar " + frameworkJarName + " $PORT",
-          1,
-          Map(applicationEnvs.toList: _*),
-          List(restUrl.toString))
-
-        Right(startApplication(request))
+        Left(true)
       }
+    } else {
+      var applicationEnvs = Map(
+        "MONGO_HOST" -> ConnectionConstants.mongoHost,
+        "MONGO_PORT" -> s"${ConnectionConstants.mongoPort}",
+        "INSTANCE_ID" -> instance.name,
+        "MESOS_MASTER" -> mesosMaster
+      )
+      if (instance.environmentVariables != null) {
+        applicationEnvs = applicationEnvs ++ Map(instance.environmentVariables.asScala.toList: _*)
+      }
+      val request = new MarathonRequest(instance.name,
+        "java -jar " + frameworkJarName + " $PORT",
+        1,
+        Map(applicationEnvs.toList: _*),
+        List(restUrl.toString))
+
+      Right(startApplication(request))
+    }
   }
 
   /**
-    * Starting generators for streams of instance
-    *
-    * @param streams - Streams
-    * @return - Future of started generators
-    */
+   * Starting generators for streams of instance
+   *
+   * @param streams - Streams
+   * @return - Future of started generators
+   */
   def startGenerators(streams: Set[TStreamSjStream]) = {
     streams.foreach { stream =>
       val generatorName = stream.name
@@ -218,11 +231,11 @@ class InstanceStarter(instance: Instance, delay: Long) extends Runnable {
   }
 
   /**
-    * Starting transaction generator for stream on mesos
-    *
-    * @param stream - Stream for running generator
-    * @return - Future with response from request to marathon
-    */
+   * Starting transaction generator for stream on mesos
+   *
+   * @param stream - Stream for running generator
+   * @return - Future with response from request to marathon
+   */
   def startGenerator(stream: TStreamSjStream) = {
     logger.debug(s"Instance: ${instance.name}. Start generator for stream ${stream.name}.")
     val transactionGeneratorJar = ConfigUtils.getTransactionGeneratorJarName()
