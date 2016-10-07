40418ab14627fdd2233e569dda5ae985011d7c6b
mikhaljova_ka <buterffly@sibmail.com>
2016-10-07 16:01:53 +0700

    add specification.json of windowed streaming stub

---
+++ contrib/stubs/sj-stub-windowed-streaming/src/main/scala/com/bwsw/sj/stubs/module/windowed_streaming/Executor.scala
@@ -0,0 +1,9 @@
+package com.bwsw.sj.stubs.module.windowed_streaming
+
+import com.bwsw.sj.engine.core.environment.RegularEnvironmentManager
+import com.bwsw.sj.engine.core.windowed.WindowedStreamingExecutor
+
+
+class Executor(manager: RegularEnvironmentManager) extends WindowedStreamingExecutor() {
+
+}
