302c4413e7ce660d75c56af5134ce7cdd7a6b108
Dima Diryavkin <diryavkin_dn@bw-sw.com>
2017-01-17 18:10:38 +0700

    SJ-2902 refactor process demo module

--- contrib/examples/sj-sflow-demo/sflow-process/src/main/scala/com/bwsw/sj/examples/sflow/module/process/mapreduce/reducerFactories/AsReducerFactory.scala
+++ contrib/examples/sj-sflow-demo/sflow-process/src/main/scala/com/bwsw/sj/examples/sflow/module/process/mapreduce/reducerFactories/AsReducerFactory.scala
@@ -1,11 +1,11 @@
 package com.bwsw.sj.examples.sflow.module.process.mapreduce.reducerFactories
 
 import com.hazelcast.mapreduce.ReducerFactory
-import com.bwsw.sj.examples.sflow.module.process.mapreduce.reducers.AsReducer
+import com.bwsw.sj.examples.sflow.module.process.mapreduce.reducers.AsIpReducer
 
 
 class AsReducerFactory extends ReducerFactory[Int, Int, Int] {
   override def newReducer(key: Int) = {
-    new AsReducer()
+    new AsIpReducer()
   }
 }
