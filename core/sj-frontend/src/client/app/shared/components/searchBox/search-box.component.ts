7712a4ef06fb368b93f4715ad38a5c3660154666
ksenya bauer <bauer_ks@bw-sw.com>
2017-02-22 11:10:54 +0700

    search-box styling

--- core/sj-frontend/src/client/app/shared/components/searchBox/search-box.component.ts
+++ core/sj-frontend/src/client/app/shared/components/searchBox/search-box.component.ts
@@ -3,7 +3,8 @@ import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'sj-search-box',
-  templateUrl: 'search-box.component.html'
+  templateUrl: 'search-box.component.html',
+  styleUrls: ['search-box.component.css']
 })
 export class SearchBoxComponent implements OnInit {
   @Output() public update = new EventEmitter();
