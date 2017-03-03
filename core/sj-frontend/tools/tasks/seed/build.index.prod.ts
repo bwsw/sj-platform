b8ba8973bc08aa87958cb7bf78a8e1f23ba1c9ae
ksenya bauer <bauer_ks@bw-sw.com>
2017-03-03 12:25:47 +0700

    -- try to fix build errors

--- core/sj-frontend/tools/tasks/seed/build.index.prod.ts
+++ core/sj-frontend/tools/tasks/seed/build.index.prod.ts
@@ -14,8 +14,11 @@ const plugins = <any>gulpLoadPlugins();
  */
 export = () => {
   return gulp.src(join(Config.APP_SRC, 'index.html'))
-    .pipe(injectJs())
-    .pipe(injectCss())
+    // .pipe(injectJs())
+    // .pipe(injectCss())
+    .pipe(inject('shims'))
+    .pipe(inject('libs'))
+    .pipe(inject())
     .pipe(plugins.template(templateLocals()))
     .pipe(gulp.dest(Config.APP_DEST));
 };
@@ -24,26 +27,47 @@ export = () => {
  * Injects the given file array and transforms the path of the files.
  * @param {Array<string>} files - The files to be injected.
  */
-function inject(...files: Array<string>) {
+/*function inject(...files: Array<string>) {
     return plugins.inject(gulp.src(files, { read: false }), {
         files,
         transform: transformPath()
     });
+}*/
+
+function inject(name?: string) {
+  return plugins.inject(gulp.src(getInjectablesDependenciesRef(name), { read: false }), {
+    name,
+    transform: transformPath()
+  });
+}
+function getInjectablesDependenciesRef(name?: string) {
+  return Config.DEPENDENCIES
+    .filter(dep => dep['inject'] && dep['inject'] === (name || true))
+    .map(mapPath);
+}
+function mapPath(dep: any) {
+  let envPath = dep.src;
+  if (envPath.startsWith(Config.APP_SRC) && !envPath.endsWith('.scss')) {
+    envPath = join(Config.APP_DEST, envPath.replace(Config.APP_SRC, ''));
+  } else if (envPath.startsWith(Config.APP_SRC) && envPath.endsWith('.scss')) {
+    envPath = envPath.replace(Config.ASSETS_SRC, Config.CSS_DEST).replace('.scss', '.css');
+  }
+  return envPath;
 }
 
 /**
  * Injects the bundled JavaScript shims and application bundles for the production environment.
  */
-function injectJs() {
+/*function injectJs() {
   return inject(join(Config.JS_DEST, Config.JS_PROD_SHIMS_BUNDLE), join(Config.JS_DEST, Config.JS_PROD_APP_BUNDLE));
-}
+}*/
 
 /**
  * Injects the bundled CSS files for the production environment.
  */
-function injectCss() {
+/*function injectCss() {
   return inject(join(Config.CSS_DEST, Config.CSS_PROD_BUNDLE));
-}
+}*/
 
 /**
  * Transform the path of a dependency to its location within the `dist` directory according to the applications
