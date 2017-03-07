9f215c8ca721d05e4cdab9631163d1761a8feba8
ksenya bauer <bauer_ks@bw-sw.com>
2017-03-07 15:00:28 +0700

    -- size transforming

--- core/sj-frontend/src/client/app/shared/services/crud-file.service.ts
+++ core/sj-frontend/src/client/app/shared/services/crud-file.service.ts
@@ -77,13 +77,12 @@ export abstract class CrudFileService<M extends BaseModel> extends BaseService<M
       .catch(this.handleError);
   }
 
-  public sizeView(size: number): string {
-    if (size/(1024*1024) >= 1) {
-      return Math.round(size/(1024*1024)) + ' Mb';
-    } else if (size/(1024) >= 1) {
-      return Math.round(size/(1024)) + ' Kb';
-    } else {
-      return Math.round(size) + ' b';
-    }
+  public sizeView(size: number, decimals: number): string {
+    if(size == 0) return '0 Bytes';
+    let k = 1000;
+    let dm = decimals + 1 || 3;
+    let sizes = ['Bytes', 'KB', 'MB'];
+    let i = Math.floor(Math.log(size) / Math.log(k));
+    return parseFloat((size / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 }
