775497dd30822d2a151129b176142469ab4c4d18
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-03-15 11:28:04 +0700

    SJ-3383 improve validator

--- contrib/sj-platform/sj-csv-input/src/main/scala/com/bwsw/sj/module/input/csv/CSVInputValidator.scala
+++ contrib/sj-platform/sj-csv-input/src/main/scala/com/bwsw/sj/module/input/csv/CSVInputValidator.scala
@@ -3,6 +3,7 @@ package com.bwsw.sj.module.input.csv
 import java.nio.charset.Charset
 
 import com.bwsw.sj.common.engine.StreamingValidator
+import com.bwsw.sj.common.utils.ValidationUtils._
 
 /**
   * Validator for csv input module.
@@ -12,42 +13,16 @@ import com.bwsw.sj.common.engine.StreamingValidator
 class CSVInputValidator extends StreamingValidator {
 
   override def validate(options: Map[String, Any]) = {
-    def isRequired(key: String) = {
-      val value = options.get(key)
-      value.nonEmpty && isString(value.get)
-    }
-
-    def isOption(key: String) = {
-      val value = options.get(key)
-      value.isEmpty || isString(value.get)
-    }
-
-    def isString(a: Any) = a.isInstanceOf[String] && a.asInstanceOf[String].nonEmpty
-
-    def checkFields = {
-      val fieldsValue = options.get(CSVInputOptionNames.fields)
-      fieldsValue match {
-        case Some(fieldNames: Seq[Any]) =>
-          fieldNames.nonEmpty && {
-            val uniqueKeyValue = options.get(CSVInputOptionNames.uniqueKey)
-            uniqueKeyValue match {
-              case Some(uniqueFields: Seq[Any]) =>
-                uniqueFields.nonEmpty && uniqueFields.forall(fieldNames.contains)
-              case None => true
-              case _ => false
-            }
-          }
-        case _ => false
-      }
-    }
-
-    isRequired(CSVInputOptionNames.lineSeparator) &&
-      isRequired(CSVInputOptionNames.encoding) &&
+    isRequired(isString)(options, CSVInputOptionNames.lineSeparator) &&
+      isRequired(isString)(options, CSVInputOptionNames.encoding) &&
       Charset.isSupported(options(CSVInputOptionNames.encoding).asInstanceOf[String]) &&
-      isRequired(CSVInputOptionNames.outputStream) &&
-      isRequired(CSVInputOptionNames.fallbackStream) &&
-      isOption(CSVInputOptionNames.fieldSeparator) &&
-      isOption(CSVInputOptionNames.quoteSymbol) &&
-      checkFields
+      isRequired(isString)(options, CSVInputOptionNames.outputStream) &&
+      isRequired(isString)(options, CSVInputOptionNames.fallbackStream) &&
+      isOption(isString)(options, CSVInputOptionNames.fieldSeparator) &&
+      isOption(isString)(options, CSVInputOptionNames.quoteSymbol) &&
+      checkAvroFields(
+        options,
+        CSVInputOptionNames.fields,
+        Seq(CSVInputOptionNames.uniqueKey, CSVInputOptionNames.distribution))
   }
 }
