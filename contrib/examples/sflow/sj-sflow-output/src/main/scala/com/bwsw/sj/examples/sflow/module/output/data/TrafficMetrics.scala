4643ccdf988c28c3e693826290ee01871e6d9af5
Dima Diryavkin <diryavkin_dn@bw-sw.com>
2017-03-15 17:46:26 +0700

    SJ-3350 refactor output modules

--- contrib/examples/sflow/sj-sflow-output/src/main/scala/com/bwsw/sj/examples/sflow/module/output/data/TrafficMetrics.scala
+++ contrib/examples/sflow/sj-sflow-output/src/main/scala/com/bwsw/sj/examples/sflow/module/output/data/TrafficMetrics.scala
@@ -1,7 +1,8 @@
 package com.bwsw.sj.examples.sflow.module.output.data
 
 import java.util.Date
-import com.bwsw.sj.engine.core.entities.EsEnvelope
+
+import com.bwsw.sj.engine.core.entities.{EsEnvelope, OutputEnvelope}
 import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
@@ -9,15 +10,18 @@ import com.fasterxml.jackson.annotation.JsonProperty
   *
   * @author Kseniya Mikhaleva
   */
-class TrafficMetrics extends EsEnvelope {
+class TrafficMetrics extends OutputEnvelope {
   var ts: Date = null
   @JsonProperty("src-as") var srcAs: Int= 0
   @JsonProperty("dst-as") var dstAs: String= null
   @JsonProperty("sum-of-traffic") var trafficSum: Long= 0
 
-  override def getDateFields(): Array[String] = {
-    val fields = super.getDateFields().toBuffer
-    fields.append("ts")
-    fields.toArray
+  def getMapFields: Map[String, AnyRef] = {
+    Map(
+      "ts" -> ts,
+      "src-as" -> srcAs,
+      "dst-as" -> dstAs,
+      "sum-of-traffic" -> trafficSum
+    )
   }
 }
--- contrib/stubs/sj-stub-es-output-streaming/src/main/scala/com/bwsw/sj/stubs/module/output/data/StubEsData.scala
+++ contrib/stubs/sj-stub-es-output-streaming/src/main/scala/com/bwsw/sj/stubs/module/output/data/StubEsData.scala
@@ -2,19 +2,17 @@ package com.bwsw.sj.stubs.module.output.data
 
 import java.util.Date
 
-import com.bwsw.sj.engine.core.entities.EsEnvelope
+import com.bwsw.sj.engine.core.entities.{EsEnvelope, OutputEnvelope}
 import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
   * @author Kseniya Tomskikh
   */
-class StubEsData extends EsEnvelope {
+class StubEsData extends OutputEnvelope {
   @JsonProperty("test-date") var testDate: Date = null
   var value: Int = 0
 
-  override def getDateFields(): Array[String] = {
-    val fields = super.getDateFields().toBuffer
-    fields.append("test-date")
-    fields.toArray
+  override def getMapFields: Map[String, AnyRef] = {
+    Map("test-date" -> testDate, "value" -> value)
   }
 }
