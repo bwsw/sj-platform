0d645f47dab2998187afa35fcbba048b7d50e8d6
Dima Diryavkin <diryavkin_dn@bw-sw.com>
2017-03-15 12:54:50 +0700

    SJ-3350 change OutputProcessor eith new realize

--- core/sj-output-streaming-engine/src/main/scala/com/bwsw/sj/engine/output/processing/OutputProcessor.scala
+++ core/sj-output-streaming-engine/src/main/scala/com/bwsw/sj/engine/output/processing/OutputProcessor.scala
@@ -2,11 +2,10 @@ package com.bwsw.sj.engine.output.processing
 
 import com.bwsw.sj.common.DAL.model.SjStream
 import com.bwsw.sj.common.utils.StreamLiterals
-import com.bwsw.sj.engine.core.entities.{Envelope, TStreamEnvelope}
+import com.bwsw.sj.engine.core.entities.{Envelope, EsEnvelope, OutputEnvelope, TStreamEnvelope}
 import com.bwsw.sj.engine.output.task.OutputTaskManager
 import com.bwsw.sj.engine.output.task.reporting.OutputStreamingPerformanceMetrics
 import org.slf4j.LoggerFactory
-import com.bwsw.sj.engine.core.output.Entity
 
 /**
   * This class used in OutputTaskEngine for sending data to different storage.
@@ -23,22 +22,22 @@ abstract class OutputProcessor[T <: AnyRef](outputStream: SjStream,
     * @param inputEnvelope      : received envelope
     * @param wasFirstCheckpoint : boolean
     */
-  def process(envelopes: List[Envelope], inputEnvelope: TStreamEnvelope[T], wasFirstCheckpoint: Boolean, entity: Entity[AnyRef]) = {
+  def process(envelopes: List[OutputEnvelope], inputEnvelope: TStreamEnvelope[T], wasFirstCheckpoint: Boolean) = {
     logger.debug("Process a set of envelopes that should be sent to output of specific type.")
-    if (!wasFirstCheckpoint) remove(inputEnvelope, entity)
-    envelopes.foreach(envelope => registerAndSendEnvelope(envelope, inputEnvelope, entity))
+    if (!wasFirstCheckpoint) remove(inputEnvelope)
+    envelopes.foreach(envelope => registerAndSendEnvelope(envelope, inputEnvelope))
   }
 
-  def remove(envelope: TStreamEnvelope[T], entity: Entity[AnyRef])
+  def remove(envelope: TStreamEnvelope[T])
 
   def close()
 
   /**
     * Registration envelope in performance metrics, and then sending to storage
     */
-  private def registerAndSendEnvelope(outputEnvelope: Envelope, inputEnvelope: TStreamEnvelope[T], entity: Entity[AnyRef]) = {
+  private def registerAndSendEnvelope(outputEnvelope: OutputEnvelope, inputEnvelope: TStreamEnvelope[T]) = {
     registerOutputEnvelope(inputEnvelope.id.toString.replaceAll("-", ""), outputEnvelope)
-    send(outputEnvelope, inputEnvelope, entity)
+    send(outputEnvelope, inputEnvelope)
   }
 
   /**
@@ -47,7 +46,7 @@ abstract class OutputProcessor[T <: AnyRef](outputStream: SjStream,
     * @param envelopeID : envelope identifier
     * @param data       : processed envelope
     */
-  private def registerOutputEnvelope(envelopeID: String, data: Envelope) = {
+  private def registerOutputEnvelope(envelopeID: String, data: OutputEnvelope) = {
     logger.debug(s"Register an output envelope: '$envelopeID'.")
     val elementSize = data.toString.length //todo придумать другой способ извлечения информации
     performanceMetrics.addElementToOutputEnvelope(outputStream.name, envelopeID, elementSize)
@@ -59,16 +58,16 @@ abstract class OutputProcessor[T <: AnyRef](outputStream: SjStream,
     * @param envelope      : processed envelope
     * @param inputEnvelope : received envelope
     */
-  protected def send(envelope: Envelope, inputEnvelope: TStreamEnvelope[T], entity: Entity[AnyRef])
+  protected def send(envelope: OutputEnvelope, inputEnvelope: TStreamEnvelope[T])
 }
 
 object OutputProcessor {
-  def apply[T <: AnyRef](outputStream: SjStream, performanceMetrics: OutputStreamingPerformanceMetrics, manager: OutputTaskManager) = {
+  def apply[T <: AnyRef](outputStream: SjStream, performanceMetrics: OutputStreamingPerformanceMetrics, manager: OutputTaskManager, entity: AnyRef) = {
     outputStream.streamType match {
       case StreamLiterals.esOutputType =>
-        new EsOutputProcessor[T](outputStream, performanceMetrics, manager)
+        new EsOutputProcessor[T](outputStream, performanceMetrics, manager, entity)
       case StreamLiterals.jdbcOutputType =>
-        new JdbcOutputProcessor[T](outputStream, performanceMetrics)
+        new JdbcOutputProcessor[T](outputStream, performanceMetrics, manager, entity)
     }
   }
 }
