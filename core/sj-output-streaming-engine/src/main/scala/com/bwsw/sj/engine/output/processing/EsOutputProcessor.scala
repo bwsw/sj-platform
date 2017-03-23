05a704e0c4b7b619ab8582a6234b9236f23d7721
Dima Diryavkin <diryavkin_dn@bw-sw.com>
2017-03-23 15:53:44 +0700

    SJ- remove commented code

--- core/sj-output-streaming-engine/src/main/scala/com/bwsw/sj/engine/output/processing/EsOutputProcessor.scala
+++ core/sj-output-streaming-engine/src/main/scala/com/bwsw/sj/engine/output/processing/EsOutputProcessor.scala
@@ -1,7 +1,7 @@
 package com.bwsw.sj.engine.output.processing
 
 
-import com.bwsw.common.{ElasticsearchClient, JsonSerializer}
+import com.bwsw.common.ElasticsearchClient
 import com.bwsw.sj.common.DAL.model.{ESService, SjStream}
 import com.bwsw.sj.engine.core.entities.{OutputEnvelope, TStreamEnvelope}
 import com.bwsw.sj.engine.output.task.OutputTaskManager
@@ -16,13 +16,9 @@ class EsOutputProcessor[T <: AnyRef](outputStream: SjStream,
                                      entity: AnyRef)
   extends OutputProcessor[T](outputStream, performanceMetrics) {
 
-  // TODO Here use es command builder for create string
-
-  private val jsonSerializer = new JsonSerializer()
   private val esService = outputStream.service.asInstanceOf[ESService]
   private val esClient = openConnection()
   private val esCommandBuilder = new ElasticsearchCommandBuilder("txn", entity.asInstanceOf[Entity[String]])
-//  prepareIndex()
   
   private def openConnection(): ElasticsearchClient = {
     logger.info(s"Open a connection to elasticsearch at address: '${esService.provider.hosts}'.")
@@ -35,31 +31,6 @@ class EsOutputProcessor[T <: AnyRef](outputStream: SjStream,
 
     (parts(0), parts(1).toInt)
   }
-
-//  def prepareIndex() = {
-//    logger.debug(s"Prepare an elasticsearch index.")
-//    esService.prepare()
-//    createIndexMapping()
-//  }
-
-//  private def createIndexMapping() = {
-//    val index = esService.index
-//    logger.debug(s"Create a mapping for an elasticsearch index: '$index'.")
-//    val streamName = outputStream.name
-//    val mappingSource = createMappingSource()
-//
-//    esClient.createMapping(index, streamName, mappingSource)
-//  }
-
-//  private def createMappingSource() = {
-//    // TODO 3350 replace mapping create to CommandBuilder
-//    logger.debug(s"Create a mapping source for an elasticsearch index.")
-//    val fields = entity.asInstanceOf[Entity[String]].getFields
-//    val mapping = Map("properties" -> fields)
-//    val mappingSource = jsonSerializer.serialize(mapping)
-//
-//    mappingSource
-//  }
 
   def remove(envelope: TStreamEnvelope[T]) = {
     val transaction = envelope.id
