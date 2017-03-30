dc2e7f094fdbbbb3c3c4dfe2588efb481f729a86
Dima Diryavkin <diryavkin_dn@bw-sw.com>
2017-03-30 10:37:04 +0700

    SJ- add exists test for JdbcCommandBuilder

--- core/sj-engine-core/src/test/scala-2.12/com/bwsw/sj/output/types/jdbc/JdbcCommandBuilderTests.scala
+++ core/sj-engine-core/src/test/scala-2.12/com/bwsw/sj/output/types/jdbc/JdbcCommandBuilderTests.scala
@@ -11,12 +11,13 @@ import org.scalatest.{FlatSpec, Matchers}
   * Created by diryavkin_dn on 07.03.17.
   */
 class JdbcCommandBuilderTests extends FlatSpec with Matchers {
-  object jdbcMock extends BasicJDBCTestCaseAdapter {
+  class  JdbcMock(sql: String) extends BasicJDBCTestCaseAdapter {
     val connection = getJDBCMockObjectFactory.getMockConnection
-    val stmt = connection.prepareStatement("")
+    val stmt = connection.prepareStatement(sql)
   }
 
   it should "work properly" in {
+    val jdbcMock = new JdbcMock("INSERT ")
     val eb = new EntityBuilder[(PreparedStatement, Int) => Unit]()
 
     val e = eb
@@ -26,7 +27,7 @@ class JdbcCommandBuilderTests extends FlatSpec with Matchers {
       .field(new BooleanField("married", false))
       .build()
 
-    val jdbccd = new JdbcCommandBuilder("txn", e)
+    val jdbccb = new JdbcCommandBuilder("txn", e)
 
     val data = Map[String, Object]().empty +
       ("id" -> new java.lang.Long(0)) +
@@ -34,6 +35,22 @@ class JdbcCommandBuilderTests extends FlatSpec with Matchers {
       ("age" -> new java.lang.Integer(32)) +
       ("married" -> new java.lang.Boolean(true))
 
-    jdbccd.buildInsert(1, data, jdbcMock.stmt).isInstanceOf[PreparedStatement] shouldBe true
+    jdbccb.buildInsert(1, data, jdbcMock.stmt).isInstanceOf[PreparedStatement] shouldBe true
+  }
+
+  it should "exists work" in {
+    val jdbcMock = new JdbcMock("SELECT * FROM table WHERE id = ?")
+    val eb = new EntityBuilder[(PreparedStatement, Int) => Unit]()
+
+    val e = eb
+      .field(new LongField("id"))
+      .field(new JavaStringField("name"))
+      .field(new IntegerField("age"))
+      .field(new BooleanField("married", false))
+      .build()
+
+    val jdbccb = new JdbcCommandBuilder("txn", e)
+
+    jdbccb.exists(1, jdbcMock.stmt) shouldBe false
   }
 }
