eb776377fc3702b5af28c5edb3564e8b314f7f6c
ksenya bauer <bauer_ks@bw-sw.com>
2017-04-04 11:54:08 +0700

    SJ-3046 - merge

--- core/sj-frontend/src/client/app/shared/models/service.model.ts
+++ core/sj-frontend/src/client/app/shared/models/service.model.ts
@@ -9,21 +9,10 @@ export class ServiceModel extends BaseModel {
   keyspace: string;
   namespace: string;
   provider: string;
-<<<<<<< HEAD
-  metadataProvider: string;
-  metadataNamespace: string;
-  dataProvider: string;
-  dataNamespace: string;
-  lockProvider: string;
-  lockNamespace: string;
   zkProvider: string;
   zkNamespace: string;
-=======
   prefix: string;
   token: string;
-  'zk-provider': string;
-  'zk-namespace': string;
->>>>>>> 01640c1d319f8c567c3e95f4153f5eb27f1e8945
   index: string;
   login: string;
   password: string;
