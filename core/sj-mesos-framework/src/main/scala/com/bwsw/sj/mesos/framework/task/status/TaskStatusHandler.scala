6e1f189dbe0a11d1147fb2ea5796728417b9586e
Dima Diryavkin <diryavkin_dn@bw-sw.com>
2017-04-13 12:32:51 +0700

    SJ- remove TODO from TaskStatusHandler.

--- core/sj-mesos-framework/src/main/scala/com/bwsw/sj/mesos/framework/task/status/TaskStatusHandler.scala
+++ core/sj-mesos-framework/src/main/scala/com/bwsw/sj/mesos/framework/task/status/TaskStatusHandler.scala
@@ -4,7 +4,6 @@ import org.apache.mesos.Protos.TaskStatus
 
 abstract class TaskStatusHandler {
   protected var status: TaskStatus
-  //TODO must be implemented and provided in inheritable class
   def setStatus(status: TaskStatus): TaskStatusHandler = {
     this.status = status
     this
