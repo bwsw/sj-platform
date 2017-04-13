a79ed1243255dc65d43065b4d02adb15f3cc21ca
mikhaljova_ka <buterffly@sibmail.com>
2017-04-13 12:02:31 +0700

    JdbcClient refactoring (in general add a jdbc client interface and jdbc mock for easer testing)

--- core/sj-engine-core/src/main/scala/com/bwsw/sj/engine/core/output/OutputStreamingExecutor.scala
+++ core/sj-engine-core/src/main/scala/com/bwsw/sj/engine/core/output/OutputStreamingExecutor.scala
@@ -1,7 +1,7 @@
 package com.bwsw.sj.engine.core.output
 
 import com.bwsw.sj.common.engine.StreamingExecutor
-import com.bwsw.sj.engine.core.entities.{Envelope, OutputEnvelope, TStreamEnvelope}
+import com.bwsw.sj.engine.core.entities.{OutputEnvelope, TStreamEnvelope}
 import com.bwsw.sj.engine.core.environment.OutputEnvironmentManager
 
 /**
@@ -16,10 +16,6 @@ abstract class OutputStreamingExecutor[T <: AnyRef](manager: OutputEnvironmentMa
     * it is invoked for every received message from one of the inputs that are defined within the instance.
     * Inside the method you have an access to the message that has the TStreamEnvelope type.
     * By extension a t-stream envelope should be transformed to output envelopes.
-    * The output envelope can have an elasticsearch type (this such of type has only one field - data
-    * that has to be an OutputData data type,You should create a class implementing OutputData
-    * and define such fields that will be in congruence with an ES index document structure)
-    * or JDBC type.
     *
     */
   def onMessage(envelope: TStreamEnvelope[T]): List[OutputEnvelope] = {
