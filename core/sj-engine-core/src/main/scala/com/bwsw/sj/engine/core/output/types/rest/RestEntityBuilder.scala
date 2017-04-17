e7449ab7dc97e4de710c30f12bbbe3749b49f30f
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-04-17 15:47:01 +0700

    SJ-3638 fix RestEntityBuilder

--- core/sj-engine-core/src/main/scala/com/bwsw/sj/engine/core/output/types/rest/RestEntityBuilder.scala
+++ core/sj-engine-core/src/main/scala/com/bwsw/sj/engine/core/output/types/rest/RestEntityBuilder.scala
@@ -5,5 +5,5 @@ import com.bwsw.sj.engine.core.output.{EntityBuilder, NamedType}
 /**
   * @author Pavel Tomskikh
   */
-class RestEntityBuilder(m: Map[String, NamedType[String]] = Map[String, NamedType[String]]())
-  extends EntityBuilder[String](m)
+class RestEntityBuilder(m: Map[String, NamedType[Any]] = Map[String, NamedType[Any]]())
+  extends EntityBuilder[Any](m)
