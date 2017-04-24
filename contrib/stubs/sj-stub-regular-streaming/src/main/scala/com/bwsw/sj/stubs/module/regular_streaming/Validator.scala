c9a31ee872d52d28d0f80eeee954520e2cdf72d5
mikhaljova_ka <buterffly@sibmail.com>
2017-04-24 15:57:22 +0700

    update t-streams version to 2.2.0

--- contrib/stubs/sj-stub-regular-streaming/src/main/scala/com/bwsw/sj/stubs/module/regular_streaming/Validator.scala
+++ contrib/stubs/sj-stub-regular-streaming/src/main/scala/com/bwsw/sj/stubs/module/regular_streaming/Validator.scala
@@ -1,5 +1,6 @@
 package com.bwsw.sj.stubs.module.regular_streaming
 
+import com.bwsw.common.JsonSerializer
 import com.bwsw.sj.common.engine.{StreamingValidator, ValidationInfo}
 
 import scala.collection.mutable.ArrayBuffer
@@ -7,6 +8,7 @@ import scala.collection.mutable.ArrayBuffer
 class Validator extends StreamingValidator {
 
   override def validate(options: String): ValidationInfo = {
-    ValidationInfo(options.nonEmpty, ArrayBuffer("Options have to be non-empty."))
+    val serializer = new JsonSerializer()
+    ValidationInfo(serializer.deserialize[Map[String, Any]](options).nonEmpty, ArrayBuffer("Options have to be non-empty."))
   }
 }
