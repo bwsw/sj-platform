87a15ae4de8343232227092fd4cfff2015cf7977
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-04-24 13:00:59 +0700

    SJ-3600 remove JDBC drivers from dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,8 +33,6 @@ object Dependencies {
       .exclude("com.fasterxml.jackson.core", "jackson-core"),
     "org.apache.logging.log4j" % "log4j-core" % "2.7",
     "org.apache.logging.log4j" % "log4j-api" % "2.7",
-    "postgresql" % "postgresql" % "9.1-901.jdbc4",
-    "mysql" % "mysql-connector-java" % "5.1.6",
     "com.maxmind.geoip" % "geoip-api" % "1.3.1",
     "io.netty" % "netty-all" % "4.1.7.Final",
     "com.opencsv" % "opencsv" % "3.9",
