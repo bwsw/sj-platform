ff896492607b2f945ffe7a3d94cad94c3a02ca1d
Dima Diryavkin <diryavkin_dn@bw-sw.com>
2017-05-04 12:22:11 +0700

    SJ-3670 Change /core/sj-common/common

--- core/sj-common/src/main/scala/com/bwsw/common/JsonSerializer.scala
+++ core/sj-common/src/main/scala/com/bwsw/common/JsonSerializer.scala
@@ -36,8 +36,8 @@ class JsonSerializer extends Serializer {
     mapper.readValue(value, typeReference[T])
   }
 
-  private def typeReference[T: Manifest] = new TypeReference[T] {
-    override def getType = typeFromManifest(manifest[T])
+  private def typeReference[T: Manifest]: TypeReference[T] = new TypeReference[T] {
+    override def getType: Type = typeFromManifest(manifest[T])
   }
 
   private def typeFromManifest(m: Manifest[_]): Type = {
@@ -45,11 +45,11 @@ class JsonSerializer extends Serializer {
       m.runtimeClass
     }
     else new ParameterizedType {
-      def getRawType = m.runtimeClass
+      def getRawType: Class[_] = m.runtimeClass
 
-      def getActualTypeArguments = m.typeArguments.map(typeFromManifest).toArray
+      def getActualTypeArguments: Array[Type] = m.typeArguments.map(typeFromManifest).toArray
 
-      def getOwnerType = null
+      def getOwnerType: Null = null
     }
   }
 
