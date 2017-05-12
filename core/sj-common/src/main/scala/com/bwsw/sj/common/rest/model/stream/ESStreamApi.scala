e4852452216a5db941159851e9ba73b1e4d65900
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-05-12 10:26:14 +0700

    SJ-3678 remove redundant methods from StreamApi

--- core/sj-common/src/main/scala/com/bwsw/sj/common/rest/model/stream/ESStreamApi.scala
+++ core/sj-common/src/main/scala/com/bwsw/sj/common/rest/model/stream/ESStreamApi.scala
@@ -1,88 +1,10 @@
 package com.bwsw.sj.common.rest.model.stream
 
-import com.bwsw.common.es.ElasticsearchClient
-import com.bwsw.sj.common.dal.model.service.ESServiceDomain
-import com.bwsw.sj.common.dal.model.stream.ESStreamDomain
-import com.bwsw.sj.common.dal.repository.ConnectionRepository
-import com.bwsw.sj.common.utils.MessageResourceUtils._
-import com.bwsw.sj.common.utils.{RestLiterals, ServiceLiterals, StreamLiterals}
-import com.fasterxml.jackson.annotation.JsonProperty
-
-import scala.collection.mutable.ArrayBuffer
+import com.bwsw.sj.common.utils.{RestLiterals, StreamLiterals}
 
 class ESStreamApi(override val name: String,
                   override val service: String,
                   override val tags: Array[String] = Array(),
                   override val force: Boolean = false,
                   override val description: String = RestLiterals.defaultDescription)
-  extends StreamApi(StreamLiterals.esOutputType, name, service, tags, force, description) {
-
-
-  override def validate(): ArrayBuffer[String] = {
-    val serviceDAO = ConnectionRepository.getServiceRepository
-    val errors = new ArrayBuffer[String]()
-
-    errors ++= super.validateGeneralFields()
-
-    Option(this.service) match {
-      case None =>
-        errors += createMessage("entity.error.attribute.required", "Service")
-      case Some(x) =>
-        if (x.isEmpty) {
-          errors += createMessage("entity.error.attribute.required", "Service")
-        }
-        else {
-          val serviceObj = serviceDAO.get(x)
-          serviceObj match {
-            case None =>
-              errors += createMessage("entity.error.doesnot.exist", "Service", x)
-            case Some(someService) =>
-              if (someService.serviceType != ServiceLiterals.elasticsearchType) {
-                errors += createMessage("entity.error.must.one.type.other.given",
-                  s"Service for '${StreamLiterals.esOutputType}' stream",
-                  ServiceLiterals.elasticsearchType,
-                  someService.serviceType)
-              }
-          }
-        }
-    }
-
-    errors
-  }
-
-  override def asModelStream(): ESStreamDomain = {
-    val serviceDAO = ConnectionRepository.getServiceRepository
-    val modelStream = new ESStreamDomain(
-      this.name,
-      serviceDAO.get(this.service).get.asInstanceOf[ESServiceDomain],
-      this.description,
-      this.force,
-      this.tags
-    )
-
-    modelStream
-  }
-
-  override def create(): Unit = {
-    if (doesStreamHaveForcedCreation()) {
-      clearEsStream()
-    }
-  }
-
-  private def doesStreamHaveForcedCreation(): Boolean = {
-    this.force
-  }
-
-  private def clearEsStream(): Unit = {
-    val serviceDAO = ConnectionRepository.getServiceRepository
-    val service = serviceDAO.get(this.service).get.asInstanceOf[ESServiceDomain]
-    val hosts = service.provider.hosts.map { host =>
-      val parts = host.split(":")
-      (parts(0), parts(1).toInt)
-    }.toSet
-    val client = new ElasticsearchClient(hosts)
-    client.deleteDocuments(service.index, this.name)
-
-    client.close()
-  }
-}
+  extends StreamApi(StreamLiterals.esOutputType, name, service, tags, force, description)
--- core/sj-common/src/main/scala/com/bwsw/sj/common/rest/model/stream/StreamApi.scala
+++ core/sj-common/src/main/scala/com/bwsw/sj/common/rest/model/stream/StreamApi.scala
@@ -1,15 +1,9 @@
 package com.bwsw.sj.common.rest.model.stream
 
-import com.bwsw.sj.common.dal.model.stream.StreamDomain
-import com.bwsw.sj.common.dal.repository.ConnectionRepository
-import com.bwsw.sj.common.rest.utils.ValidationUtils._
 import com.bwsw.sj.common.si.model.stream._
-import com.bwsw.sj.common.utils.MessageResourceUtils._
 import com.bwsw.sj.common.utils.{RestLiterals, StreamLiterals}
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type
 import com.fasterxml.jackson.annotation.{JsonIgnore, JsonProperty, JsonSubTypes, JsonTypeInfo}
-
-import scala.collection.mutable.ArrayBuffer
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = classOf[StreamApi], visible = true)
 @JsonSubTypes(Array(
@@ -29,56 +23,6 @@ class StreamApi(@JsonProperty("type") val streamType: String,
   @JsonIgnore
   def to: SjStream =
     new SjStream(streamType, name, service, tags, force, description)
-
-  @JsonIgnore
-  def create(): Unit = {}
-
-  @JsonIgnore
-  def asModelStream(): StreamDomain = ???
-
-  @JsonIgnore
-  def validate(): ArrayBuffer[String] = validateGeneralFields()
-
-  @JsonIgnore
-  protected def validateGeneralFields(): ArrayBuffer[String] = {
-    val streamDAO = ConnectionRepository.getStreamRepository
-    val errors = new ArrayBuffer[String]()
-
-    Option(this.name) match {
-      case None =>
-        errors += createMessage("entity.error.attribute.required", "Name")
-      case Some(x) =>
-        if (x.isEmpty) {
-          errors += createMessage("entity.error.attribute.required", "Name")
-        }
-        else {
-          if (!validateName(x)) {
-            errors += createMessage("entity.error.incorrect.name", "Stream", x, "stream")
-          }
-
-          val streamObj = streamDAO.get(x)
-          if (streamObj.isDefined) {
-            errors += createMessage("entity.error.already.exists", "Stream", x)
-          }
-        }
-    }
-
-    Option(this.streamType) match {
-      case None =>
-        errors += createMessage("entity.error.attribute.required", "Type")
-      case Some(t) =>
-        if (t.isEmpty) {
-          errors += createMessage("entity.error.attribute.required", "Type")
-        }
-        else {
-          if (!StreamLiterals.types.contains(t)) {
-            errors += createMessage("entity.error.unknown.type.must.one.of", t, "stream", StreamLiterals.types.mkString("[", ", ", "]"))
-          }
-        }
-    }
-
-    errors
-  }
 }
 
 object StreamApi {
@@ -144,11 +88,3 @@ object StreamApi {
       )
   }
 }
-
-
-
-
-
-
-
-
