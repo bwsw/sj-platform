578c0de4244f03a7d658011cf9e73248881ffebf
mikhaljova_ka <buterffly@sibmail.com>
2017-06-01 14:10:19 +0700

    SJ-3732 cover InputInstanceValidator with simple tests

--- core/sj-crud-rest/src/test/scala-2.12/com/bwsw/sj/crud/rest/validator/instance/InputInstanceValidatorTestSuit.scala
+++ core/sj-crud-rest/src/test/scala-2.12/com/bwsw/sj/crud/rest/validator/instance/InputInstanceValidatorTestSuit.scala
@@ -1,102 +1,59 @@
-//package com.bwsw.sj.crud.rest.validator.instance
-//
-//import com.bwsw.sj.common.dal.model.stream.StreamDomain
-//import com.bwsw.sj.common.utils.EngineLiterals
-//import org.scalatest.{FlatSpec, Matchers}
-//
-//import scala.collection.mutable.ArrayBuffer
-//
-//class InputInstanceValidatorTestSuit extends FlatSpec with Matchers {
-//  private val defaultStreamMode = EngineLiterals.splitStreamMode
-//
-//  it should "not have implementation of validate() method by default" in new InstanceValidatorMocks {
-//    //act and assert
-//    assertThrows[NotImplementedError] {
-//      instanceValidator.validate(null, null)
-//    }
-//  }
-//
-//  it should s"getStreamMode() method returns $defaultStreamMode if a stream hasn't got a stream mode" in new InstanceValidatorMocks {
-//    //arrange
-//    val streamWithoutMode = "stream_name"
-//
-//    //act
-//    val streamMode: String = instanceValidator.getStreamMode(streamWithoutMode)
-//
-//    //assert
-//    streamMode shouldBe defaultStreamMode
-//  }
-//
-//  it should s"getStreamMode() method returns a stream mode of stream that is separated of stream name by '/' " in new InstanceValidatorMocks {
-//    //arrange
-//    val streamWithoutMode = "stream_name"
-//    val actualStreamMode = "stream_mode"
-//    val streamWithMode = actualStreamMode + "/" + streamWithoutMode
-//
-//    //act
-//    val streamMode: String = instanceValidator.getStreamMode(streamWithMode)
-//
-//    //assert
-//    streamMode shouldBe actualStreamMode
-//  }
-//
-//  it should s"getStreamServices() method returns empty list if no streams are passed" in new InstanceValidatorMocks {
-//    //arrange
-//    val streams: ArrayBuffer[StreamDomain] = ArrayBuffer()
-//
-//    //act
-//    val streamServices = instanceValidator.getStreamServices(streams)
-//
-//    //assert
-//    streamServices shouldBe empty
-//  }
-//
-//  it should s"getStreamServices() method returns the set of services names" in new InstanceValidatorMocks {
-//    //arrange
-//    val services: ArrayBuffer[String] = ArrayBuffer()
-//    val streams = getStreamStorage.getAll
-//    streams.foreach(services += _.service.name)
-//
-//    //act
-//    val streamServices = instanceValidator.getStreamServices(streams)
-//
-//    //assert
-//    services should contain theSameElementsAs streamServices
-//  }
-//}
-//
-//trait InstanceValidatorMocks extends MockitoSugar {
-//  private val instanceRepositoryMock = new InstanceRepositoryMock()
-//  private val serviceRepositoryMock = new ServiceRepositoryMock()
-//  private val streamRepositoryMock = new StreamRepositoryMock()
-//
-//  private val connectionRepository = mock[ConnectionRepository]
-//  when(connectionRepository.getInstanceRepository).thenReturn(instanceRepositoryMock.repository)
-//  when(connectionRepository.getServiceRepository).thenReturn(serviceRepositoryMock.repository)
-//  when(connectionRepository.getStreamRepository).thenReturn(streamRepositoryMock.repository)
-//
-//  private val module = new Module {
-//    bind[ConnectionRepository] to connectionRepository
-//  }
-//  private val injector = module.injector
-//
-//  val instanceValidator = new InstanceValidator()(injector) {
-//    override type T = Instance
-//
-//    /**
-//      * Validating input parameters for streaming module
-//      *
-//      * @param instance - input parameters for running module
-//      * @return - List of errors
-//      */
-//    override def validate(instance: Instance, specification: Specification) = ???
-//
-//    override protected def validateStreamOptions(instance: T, specification: Specification) = ???
-//  }
-//
-//  def getServiceStorage: GenericMongoRepository[ServiceDomain] = serviceRepositoryMock.repository
-//
-//  def getStreamStorage: GenericMongoRepository[StreamDomain] = streamRepositoryMock.repository
-//
-//  def getInstanceStorage: GenericMongoRepository[InstanceDomain] = instanceRepositoryMock.repository
-//}
+package com.bwsw.sj.crud.rest.validator.instance
+
+import com.bwsw.sj.common.dal.model.service.ZKServiceDomain
+import com.bwsw.sj.common.si.model.instance.InputInstance
+import com.bwsw.sj.common.utils.EngineLiterals
+import com.bwsw.sj.crud.rest.common.SpecificationWithRandomFieldsMock
+import org.mockito.Mockito.when
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+class InputInstanceValidatorTestSuit extends FlatSpec with Matchers with InstanceValidatorMocks {
+  private val instanceValidator = new InputInstanceValidator()(injector)
+
+  it should "validate() method returns non empty set of errors if instance and specification is blank" in {
+    //arrange
+    val instance = new InputInstanceWithDefaultFieldsMock().instance
+    val specification = new SpecificationWithRandomFieldsMock().specification
+
+    //act
+    val errors = instanceValidator.validate(instance, specification)
+
+    //assert
+    errors should not be empty
+  }
+
+  it should "validate() method returns empty set of errors if instance is filled with appropriate specification" in {
+    //arrange
+    val zkServiceName = "zk_service"
+    val zkService = mock[ZKServiceDomain]
+    when(zkService.name).thenReturn(zkServiceName)
+    getServiceStorage.save(zkService)
+
+    val instance = new InputInstanceWithDefaultFieldsMock().instance
+    when(instance.coordinationService).thenReturn(zkServiceName)
+
+    val specification = new SpecificationWithRandomFieldsMock().specification
+
+    //act
+    val errors = instanceValidator.validate(instance, specification)
+
+    //assert
+    errors shouldBe empty
+  }
+}
+
+class InputInstanceWithDefaultFieldsMock() extends MockitoSugar {
+  val instance = mock[InputInstance]
+  when(instance.name).thenReturn("correct-name")
+  when(instance.outputs).thenReturn(Array[String]())
+  when(instance.perTaskCores).thenReturn(1)
+  when(instance.perTaskRam).thenReturn(1024)
+  when(instance.performanceReportingInterval).thenReturn(60000)
+  when(instance.parallelism).thenReturn(1, Nil: _*)
+  when(instance.checkpointMode).thenReturn(EngineLiterals.everyNthMode)
+  when(instance.checkpointInterval).thenReturn(1)
+  when(instance.queueMaxSize).thenReturn(271)
+  when(instance.defaultEvictionPolicy).thenReturn(EngineLiterals.noneDefaultEvictionPolicy)
+  when(instance.evictionPolicy).thenReturn(EngineLiterals.fixTimeEvictionPolicy)
+}
