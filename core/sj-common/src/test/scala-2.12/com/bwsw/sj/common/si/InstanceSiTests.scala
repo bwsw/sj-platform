800fdfc5e2d06501a4aff9856b369608867352b0
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-06-06 14:01:16 +0700

    SJ-3728 implement tests for getAll, getByModule, get, delete, canStart, canStop in InstanceSI

---
+++ core/sj-common/src/test/scala-2.12/com/bwsw/sj/common/si/InstanceSiTests.scala
@@ -0,0 +1,212 @@
+package com.bwsw.sj.common.si
+
+import com.bwsw.common.file.utils.MongoFileStorage
+import com.bwsw.sj.common.dal.model.instance.InstanceDomain
+import com.bwsw.sj.common.dal.repository.{ConnectionRepository, GenericMongoRepository}
+import com.bwsw.sj.common.si.model.instance.{Instance, InstanceConversion}
+import com.bwsw.sj.common.si.model.module.{ModuleMetadata, Specification}
+import com.bwsw.sj.common.si.result._
+import com.bwsw.sj.common.utils.EngineLiterals._
+import com.bwsw.sj.common.utils.{MessageResourceUtils, MessageResourceUtilsMock}
+import org.mockito.ArgumentMatchers.{any, anyString, eq => argEq}
+import org.mockito.Mockito.{never, reset, verify, when}
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
+import scaldi.{Injector, Module}
+
+class InstanceSiTests extends FlatSpec with Matchers with MockitoSugar with BeforeAndAfterEach {
+  val tmpDirectory = "tmp/"
+
+  val instanceConversion = mock[InstanceConversion]
+
+  val module1 = createModule(regularStreamingType, "module-1", "v1")
+  val readyInstance = createInstance(module1, "ready-instance", ready)
+  val startedInstance = createInstance(module1, "started-instance", started)
+  val failedInstance = createInstance(module1, "failed-instance", failed)
+  val module1Instances = Seq(readyInstance, startedInstance, failedInstance)
+
+  val module2 = createModule(inputStreamingType, "module-2", "v2")
+  val stoppedInstance = createInstance(module2, "stopped-instance", stopped)
+  val deletedInstance = createInstance(module2, "deleted-instance", deleted)
+  val errorInstance = createInstance(module2, "error-instance", error)
+  val module2Instances = Seq(deletedInstance, stoppedInstance, errorInstance)
+
+  val storedInstances = module1Instances ++ module2Instances
+  val canStartInstances = Seq(readyInstance, stoppedInstance, failedInstance)
+
+  val moduleToInstances = Map(module1 -> module1Instances, module2 -> module2Instances)
+
+  val instanceRepository = mock[GenericMongoRepository[InstanceDomain]]
+  val fileStorage = mock[MongoFileStorage]
+
+  val connectionRepository = mock[ConnectionRepository]
+  when(connectionRepository.getFileStorage).thenReturn(fileStorage)
+  when(connectionRepository.getInstanceRepository).thenReturn(instanceRepository)
+
+  val injector = new Module {
+    bind[ConnectionRepository] to connectionRepository
+    bind[MessageResourceUtils] to MessageResourceUtilsMock.messageResourceUtils
+    bind[InstanceConversion] to instanceConversion
+  }.injector
+
+  val instanceSI = new InstanceSI()(injector)
+
+  override def beforeEach(): Unit = {
+    reset(fileStorage)
+
+    when(fileStorage.exists(anyString())).thenReturn(false)
+    when(fileStorage.delete(anyString())).thenReturn(false)
+
+    reset(instanceRepository)
+
+    when(instanceRepository.getByParameters(any[Map[String, Any]]())).thenReturn(Seq.empty)
+    when(instanceRepository.get(anyString())).thenReturn(None)
+    when(instanceRepository.getAll).thenReturn(storedInstances.map(_.instanceDomain).toBuffer)
+    moduleToInstances.foreach {
+      case (module, instances) =>
+        when(instanceRepository.getByParameters(
+          Map(
+            "module-name" -> module.specification.name,
+            "module-type" -> module.specification.moduleType,
+            "module-version" -> module.specification.version)))
+          .thenReturn(instances.map(_.instanceDomain))
+    }
+    storedInstances.foreach {
+      case InstanceInfo(_, instanceDomain, _) =>
+        when(instanceRepository.get(instanceDomain.name)).thenReturn(Some(instanceDomain))
+    }
+  }
+
+  // getAll
+  "InstanceSI" should "give all instances from storage" in {
+    instanceSI.getAll.toSet shouldBe storedInstances.map(_.instance).toSet
+  }
+
+  // getByModule
+  it should "give all instances of specific module" in {
+    moduleToInstances.foreach {
+      case (module, instances) =>
+        val moduleType = module.specification.moduleType
+        val moduleName = module.specification.name
+        val moduleVersion = module.specification.version
+
+        instanceSI.getByModule(moduleType, moduleName, moduleVersion).toSet shouldBe instances.map(_.instance).toSet
+    }
+  }
+
+  // get
+  it should "give module if it exists" in {
+    storedInstances.foreach {
+      case InstanceInfo(instance, _, _) =>
+        instanceSI.get(instance.name) shouldBe Some(instance)
+    }
+  }
+
+  it should "not give instance if it does not exists" in {
+    instanceSI.get("not-existed-instsce") shouldBe empty
+  }
+
+  // delete
+  it should "delete instance if it has never been run and have status that does allow deletion" in {
+    val name = readyInstance.instance.name
+
+    instanceSI.delete(name) shouldBe Deleted
+    verify(instanceRepository).delete(name)
+  }
+
+  it should "mark instance to deletion if it has already been run and have status that does allow deletion" in {
+    Seq(stoppedInstance, failedInstance, errorInstance).foreach {
+      case InstanceInfo(instance, _, _) =>
+        instanceSI.delete(instance.name) shouldBe WillBeDeleted(instance)
+        verify(instanceRepository, never()).delete(anyString())
+    }
+  }
+
+  it should "not delete instance if it have status that does not allow deletion" in {
+    Seq(startedInstance, deletedInstance).foreach {
+      case InstanceInfo(instance, _, _) =>
+        val error = "rest.modules.instances.instance.cannot.delete:" + instance.name
+
+        instanceSI.delete(instance.name) shouldBe DeletionError(error)
+        verify(instanceRepository, never()).delete(anyString())
+    }
+  }
+
+  it should "not delete instance if it does not exists" in {
+    instanceSI.delete("not-existed-instance") shouldBe EntityNotFound
+    verify(instanceRepository, never()).delete(anyString())
+  }
+
+  // canStart
+  it should "correctly indicate that instance can be started" in {
+    canStartInstances.foreach {
+      case InstanceInfo(instance, _, _) =>
+        instanceSI.canStart(instance) shouldBe true
+    }
+  }
+
+  it should "correctly indicate that instance cannot be started" in {
+    storedInstances.diff(canStartInstances).foreach {
+      case InstanceInfo(instance, _, _) =>
+        instanceSI.canStart(instance) shouldBe false
+    }
+  }
+
+  // canStop
+  it should "correctly indicate that instance can be stopped" in {
+    instanceSI.canStop(startedInstance.instance) shouldBe true
+  }
+
+  it should "correctly indicate that instance cannot be stopped" in {
+    storedInstances.filter(_ != startedInstance).foreach {
+      case InstanceInfo(instance, _, _) =>
+        instanceSI.canStop(instance) shouldBe false
+    }
+  }
+
+
+  private def createModule(moduleType: String, name: String, version: String) = {
+    val specification = mock[Specification]
+    when(specification.name).thenReturn(name)
+    when(specification.version).thenReturn(version)
+    when(specification.moduleType).thenReturn(moduleType)
+
+    val module = mock[ModuleMetadata]
+    when(module.specification).thenReturn(specification)
+    when(module.name).thenReturn(Some(name))
+    when(module.version).thenReturn(Some(version))
+
+    module
+  }
+
+  private def createInstance(module: ModuleMetadata, name: String, status: String) = {
+    val instanceDomain = mock[InstanceDomain]
+
+    val moduleType = module.specification.moduleType
+    val moduleName = module.specification.name
+    val moduleVersion = module.specification.version
+
+    when(instanceDomain.moduleType).thenReturn(moduleType)
+    when(instanceDomain.moduleName).thenReturn(moduleName)
+    when(instanceDomain.moduleVersion).thenReturn(moduleVersion)
+    when(instanceDomain.name).thenReturn(name)
+    when(instanceDomain.status).thenReturn(status)
+
+    val instance = mock[Instance]
+    when(instance.moduleType).thenReturn(moduleType)
+    when(instance.moduleName).thenReturn(moduleName)
+    when(instance.moduleVersion).thenReturn(moduleVersion)
+    when(instance.name).thenReturn(name)
+    when(instance.status).thenReturn(status)
+    when(instance.to()).thenReturn(instanceDomain)
+
+    when(instanceConversion.from(argEq(instanceDomain))(any[Injector]())).thenReturn(instance)
+
+    InstanceInfo(instance, instanceDomain, module)
+  }
+
+  case class InstanceInfo(instance: Instance,
+                          instanceDomain: InstanceDomain,
+                          module: ModuleMetadata)
+
+}
