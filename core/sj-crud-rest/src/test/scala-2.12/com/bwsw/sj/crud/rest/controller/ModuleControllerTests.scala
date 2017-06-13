02214e62399c60181e6b7d176607a3ca1ae1c6af
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-06-13 12:27:11 +0700

    SJ-3722 cover delete, getRelated in ModuleController with unit tests

--- core/sj-crud-rest/src/test/scala-2.12/com/bwsw/sj/crud/rest/controller/ModuleControllerTests.scala
+++ core/sj-crud-rest/src/test/scala-2.12/com/bwsw/sj/crud/rest/controller/ModuleControllerTests.scala
@@ -8,12 +8,12 @@ import com.bwsw.common.exceptions.JsonDeserializationException
 import com.bwsw.sj.common.rest._
 import com.bwsw.sj.common.si.ModuleSI
 import com.bwsw.sj.common.si.model.module.{ModuleMetadata, Specification}
-import com.bwsw.sj.common.si.result.{Created, NotCreated}
+import com.bwsw.sj.common.si.result.{Created, Deleted, DeletionError, NotCreated}
 import com.bwsw.sj.common.utils.EngineLiterals._
 import com.bwsw.sj.common.utils.MessageResourceUtils
 import com.bwsw.sj.crud.rest.model.module.{CreateSpecificationApi, ModuleMetadataApi, SpecificationApi}
 import com.bwsw.sj.crud.rest.utils.{FileMetadataUtils, JsonDeserializationErrorMessageCreator}
-import com.bwsw.sj.crud.rest.{ModuleInfo, ModuleJar, ModulesResponseEntity, SpecificationResponseEntity}
+import com.bwsw.sj.crud.rest._
 import org.mockito.Mockito.{reset, when}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
@@ -38,16 +38,16 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
   when(messageResourceUtils.createMessage(notUploadedMessageName, newModuleFilename, uploadingError))
     .thenReturn(notUploadedMessage)
 
-  val existenceModuleType = inputStreamingType
-  val existenceModuleName = "existence-module-name"
-  val existenceModuleVersion = "existence-module-version"
-  val existenceModuleFilename = s"$existenceModuleName-$existenceModuleVersion.jar"
-  val existenceModuleFile = new File(existenceModuleFilename)
-  val existenceModule = mock[ModuleMetadata]
-  when(serviceInterface.get(existenceModuleType, existenceModuleName, existenceModuleVersion))
-    .thenReturn(Right(existenceModule))
-  when(serviceInterface.getMetadataWithoutFile(existenceModuleType, existenceModuleName, existenceModuleVersion))
-    .thenReturn(Right(existenceModule))
+  val existingModuleType = inputStreamingType
+  val existingModuleName = "existing-module-name"
+  val existingModuleVersion = "existing-module-version"
+  val existingModuleFilename = s"$existingModuleName-$existingModuleVersion.jar"
+  val existingModuleFile = new File(existingModuleFilename)
+  val existingModule = mock[ModuleMetadata]
+  when(serviceInterface.get(existingModuleType, existingModuleName, existingModuleVersion))
+    .thenReturn(Right(existingModule))
+  when(serviceInterface.getMetadataWithoutFile(existingModuleType, existingModuleName, existingModuleVersion))
+    .thenReturn(Right(existingModule))
 
   val wrongType = "wrong-type"
   val wrongName = "wrong-name"
@@ -56,28 +56,6 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
   when(serviceInterface.get(wrongType, wrongName, wrongVersion)).thenReturn(Left(moduleNotFoundError))
   when(serviceInterface.getMetadataWithoutFile(wrongType, wrongName, wrongVersion))
     .thenReturn(Left(moduleNotFoundError))
-
-  val moduleSize = 1000l
-  val inputModulesCount = 3
-  val inputModules = Range(0, inputModulesCount).map(i => createModule(s"name$i", inputStreamingType))
-  val regularModulesCount = 3
-  val regularModules = Range(0, regularModulesCount).map(i => createModule(s"name$i", regularStreamingType))
-
-  val typeToModules = Map(
-    inputStreamingType -> inputModules,
-    regularStreamingType -> regularModules,
-    outputStreamingType -> Seq.empty,
-    batchStreamingType -> Seq.empty)
-
-  val unknownTypeError = "unknown type"
-  when(serviceInterface.getByType(wrongType)).thenReturn(Left(unknownTypeError))
-  typeToModules.foreach {
-    case (moduleType, modules) =>
-      when(serviceInterface.getByType(moduleType)).thenReturn(Right(modules.map(_._2)))
-  }
-
-  val allModules = inputModules ++ regularModules
-  when(serviceInterface.getAll).thenReturn(allModules.map(_._2))
 
   val injector = new Module {
     bind[MessageResourceUtils] to messageResourceUtils
@@ -93,12 +71,12 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
     when(newModuleApi.filename).thenReturn(Some(newModuleFilename))
     when(newModule.filename).thenReturn(newModuleFilename)
 
-    when(existenceModule.filename).thenReturn(existenceModuleFilename)
-    when(existenceModule.file).thenReturn(Some(existenceModuleFile))
+    when(existingModule.filename).thenReturn(existingModuleFilename)
+    when(existingModule.file).thenReturn(Some(existingModuleFile))
   }
 
   override def afterEach(): Unit = {
-    reset(newModuleApi, newModule, existenceModule)
+    reset(newModuleApi, newModule, existingModule)
   }
 
   // create
@@ -148,11 +126,11 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
 
   // get
   it should "give module if it exist" in {
-    val source = FileIO.fromPath(Paths.get(existenceModuleFile.getAbsolutePath))
-    val expectedResponse = ModuleJar(existenceModuleFilename, source)
-    when(fileMetadataUtils.fileToSource(existenceModuleFile)).thenReturn(source)
+    val source = FileIO.fromPath(Paths.get(existingModuleFile.getAbsolutePath))
+    val expectedResponse = ModuleJar(existingModuleFilename, source)
+    when(fileMetadataUtils.fileToSource(existingModuleFile)).thenReturn(source)
 
-    controller.get(existenceModuleType, existenceModuleName, existenceModuleVersion) shouldBe expectedResponse
+    controller.get(existingModuleType, existingModuleName, existingModuleVersion) shouldBe expectedResponse
   }
 
   it should "not give module if it does not exist" in {
@@ -163,6 +141,10 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
 
   // getAll
   it should "give all modules" in {
+    val modulesCount = 5
+    val allModules = Range(0, modulesCount).map(i => createModule(s"name$i", batchStreamingType))
+    when(serviceInterface.getAll).thenReturn(allModules.map(_._2))
+
     val expectedResponse = OkRestResponse(ModulesResponseEntity(allModules.map(_._1)))
 
     controller.getAll shouldBe expectedResponse
@@ -170,8 +152,19 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
 
   // getByType
   it should "give all modules with specific type" in {
-    typeToModules.foreach {
+    val inputModulesCount = 3
+    val inputModules = Range(0, inputModulesCount).map(i => createModule(s"name$i", inputStreamingType))
+    val regularModulesCount = 3
+    val regularModules = Range(0, regularModulesCount).map(i => createModule(s"name$i", regularStreamingType))
+
+    Map(
+      inputStreamingType -> inputModules,
+      regularStreamingType -> regularModules,
+      outputStreamingType -> Seq.empty,
+      batchStreamingType -> Seq.empty).foreach {
       case (moduleType, modules) =>
+        when(serviceInterface.getByType(moduleType)).thenReturn(Right(modules.map(_._2)))
+
         val expectedResponse = OkRestResponse(ModulesResponseEntity(modules.map(_._1)))
 
         controller.getByType(moduleType) shouldBe expectedResponse
@@ -179,6 +172,9 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
   }
 
   it should "not give modules if module type is incorrect" in {
+    val unknownTypeError = "unknown type"
+    when(serviceInterface.getByType(wrongType)).thenReturn(Left(unknownTypeError))
+
     val expectedResponse = BadRequestRestResponse(MessageResponseEntity(unknownTypeError))
 
     controller.getByType(wrongType) shouldBe expectedResponse
@@ -195,12 +191,12 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
   it should "give module specification" in {
     val specification = mock[Specification]
     val specificationApi = mock[SpecificationApi]
-    when(existenceModule.specification).thenReturn(specification)
+    when(existingModule.specification).thenReturn(specification)
     when(createSpecificationApi.from(specification)).thenReturn(specificationApi)
 
     val expectedResponse = OkRestResponse(SpecificationResponseEntity(specificationApi))
 
-    controller.getSpecification(existenceModuleType, existenceModuleName, existenceModuleVersion) shouldBe expectedResponse
+    controller.getSpecification(existingModuleType, existingModuleName, existingModuleVersion) shouldBe expectedResponse
   }
 
   it should "not give module specification if module does not exist" in {
@@ -209,10 +205,61 @@ class ModuleControllerTests extends FlatSpec with Matchers with MockitoSugar wit
     controller.get(wrongType, wrongName, wrongVersion) shouldBe expectedResponse
   }
 
+  // delete
+  it should "delete module if it exists" in {
+    val moduleSignature = s"$existingModuleType-$existingModuleName-$existingModuleVersion"
+    when(existingModule.signature).thenReturn(moduleSignature)
+    when(serviceInterface.delete(existingModule)).thenReturn(Deleted)
+    val deletedMessageName = "rest.modules.module.deleted"
+    val deletedMessage = deletedMessageName + "," + moduleSignature
+    when(messageResourceUtils.createMessage(deletedMessageName, moduleSignature)).thenReturn(deletedMessage)
+
+    val expectedResponse = OkRestResponse(MessageResponseEntity(deletedMessage))
+
+    controller.delete(existingModuleType, existingModuleName, existingModuleVersion) shouldBe expectedResponse
+  }
+
+  it should "not delete module if it does not exists" in {
+    val expectedResponse = NotFoundRestResponse(MessageResponseEntity(moduleNotFoundError))
+
+    controller.delete(wrongType, wrongName, wrongVersion) shouldBe expectedResponse
+  }
+
+  it should "report in a case some deletion error" in {
+    val moduleType = batchStreamingType
+    val moduleName = "not-deleted-module-name"
+    val moduleVersion = "not-deleted-module-version"
+    val module = mock[ModuleMetadata]
+    when(serviceInterface.get(moduleType, moduleName, moduleVersion))
+      .thenReturn(Right(module))
+    val deletionError = "module not deleted"
+    when(serviceInterface.delete(module)).thenReturn(DeletionError(deletionError))
+
+    val expectedResponse = UnprocessableEntityRestResponse(MessageResponseEntity(deletionError))
+
+    controller.delete(moduleType, moduleName, moduleVersion) shouldBe expectedResponse
+  }
+
+  // getRelated
+  it should "give instances that related with module" in {
+    val relatedInstances = Seq("instance1", "instance2", "instance3")
+    when(serviceInterface.getRelatedInstances(existingModule)).thenReturn(relatedInstances)
+
+    val expectedResponse = OkRestResponse(RelatedToModuleResponseEntity(relatedInstances))
+
+    controller.getRelated(existingModuleType, existingModuleName, existingModuleVersion) shouldBe expectedResponse
+  }
+
+  it should "not give related instances if module does not exists" in {
+    val expectedResponse = NotFoundRestResponse(MessageResponseEntity(moduleNotFoundError))
+
+    controller.delete(wrongType, wrongName, wrongVersion) shouldBe expectedResponse
+  }
 
   private def createModule(name: String, moduleType: String): (ModuleInfo, ModuleMetadata) = {
     val module = mock[ModuleMetadata]
     val version = "version"
+    val moduleSize = 1000l
     val moduleInfo = ModuleInfo(moduleType, name, version, moduleSize)
 
     when(fileMetadataUtils.toModuleInfo(module)).thenReturn(moduleInfo)
