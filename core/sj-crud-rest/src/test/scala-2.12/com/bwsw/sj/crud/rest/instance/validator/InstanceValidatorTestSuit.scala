0c26438d5481c6263f376e7936c6a748b6ed7938
mikhaljova_ka <buterffly@sibmail.com>
2017-06-20 13:50:23 +0700

    delete tests related to the previous changes

--- core/sj-crud-rest/src/test/scala-2.12/com/bwsw/sj/crud/rest/instance/validator/InstanceValidatorTestSuit.scala
+++ core/sj-crud-rest/src/test/scala-2.12/com/bwsw/sj/crud/rest/instance/validator/InstanceValidatorTestSuit.scala
@@ -21,7 +21,6 @@ package com.bwsw.sj.crud.rest.instance.validator
 import com.bwsw.sj.common.dal.model.service.ZKServiceDomain
 import com.bwsw.sj.common.dal.model.stream.StreamDomain
 import com.bwsw.sj.common.si.model.instance.Instance
-import com.bwsw.sj.common.utils.EngineLiterals
 import com.bwsw.sj.crud.rest.common._
 import org.mockito.Mockito.when
 import org.scalatest.{FlatSpec, Matchers}
@@ -29,8 +28,6 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.collection.mutable.ArrayBuffer
 
 class InstanceValidatorTestSuit extends FlatSpec with Matchers with InstanceValidatorMocks {
-  private val defaultStreamMode = EngineLiterals.splitStreamMode
-
   private val instanceValidator = new InstanceValidator()(injector) {
     override type T = Instance
   }
@@ -66,30 +63,6 @@ class InstanceValidatorTestSuit extends FlatSpec with Matchers with InstanceVali
 
     //assert
     errors shouldBe empty
-  }
-
-  it should s"getStreamMode() method returns $defaultStreamMode if a stream hasn't got a stream mode" in {
-    //arrange
-    val streamWithoutMode = "stream_name"
-
-    //act
-    val streamMode: String = instanceValidator.getStreamMode(streamWithoutMode)
-
-    //assert
-    streamMode shouldBe defaultStreamMode
-  }
-
-  it should s"getStreamMode() method returns a stream mode of stream that is separated of stream name by '/' " in {
-    //arrange
-    val streamWithoutMode = "stream_name"
-    val actualStreamMode = "stream_mode"
-    val streamWithMode = actualStreamMode + "/" + streamWithoutMode
-
-    //act
-    val streamMode: String = instanceValidator.getStreamMode(streamWithMode)
-
-    //assert
-    streamMode shouldBe actualStreamMode
   }
 
   it should s"getStreamServices() method returns empty list if no streams are passed" in {
