5dddb3ba8270c6441c75e99fd7d22d40cf391578
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-06-30 11:26:34 +0700

    SJ-3850 remove unused methods from AvroRecordUtils

--- core/sj-common/src/main/scala/com/bwsw/sj/common/utils/AvroRecordUtils.scala
+++ core/sj-common/src/main/scala/com/bwsw/sj/common/utils/AvroRecordUtils.scala
@@ -18,8 +18,6 @@
  */
 package com.bwsw.sj.common.utils
 
-import com.bwsw.common.JsonSerializer
-import org.apache.avro.Schema
 import org.apache.avro.generic.GenericData.Record
 
 /**
@@ -28,8 +26,6 @@ import org.apache.avro.generic.GenericData.Record
   * @author Pavel Tomskikh
   */
 object AvroRecordUtils {
-
-  private val serializer = new JsonSerializer(true)
 
   /**
     * Returns concatenated fields values from record.
@@ -40,28 +36,4 @@ object AvroRecordUtils {
     */
   def concatFields(fieldNames: Seq[String], record: Record): String =
     fieldNames.map(field => record.get(field).toString).mkString(",")
-
-  def jsonToSchema(json: String): Option[Schema] = {
-    Option(json) match {
-      case None | Some("{}") => None
-      case Some(s) =>
-        val parser = new Schema.Parser()
-        Option(parser.parse(s))
-    }
-  }
-
-  def schemaToJson(schema: Option[Schema]): String =
-    schema.map(_.toString).getOrElse("{}")
-
-  def mapToSchema(map: Map[String, Any]): Option[Schema] = {
-    if (map.isEmpty) None
-    else {
-      val schemaJson = serializer.serialize(map)
-      val parser = new Schema.Parser()
-      Option(parser.parse(schemaJson))
-    }
-  }
-
-  def schemaToMap(schema: Option[Schema]): Map[String, Any] =
-    schema.map(_.toString).map(serializer.deserialize[Map[String, Any]]).getOrElse(Map())
 }
