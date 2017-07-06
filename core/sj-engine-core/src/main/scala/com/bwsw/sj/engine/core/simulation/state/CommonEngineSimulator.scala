0b4e93f953a6ef4f4c09717319847be332f95208
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-07-06 16:14:31 +0700

    SJ-3883 move some methods from RegularEngineSimulator to CommonEngineSimulator

---
+++ core/sj-engine-core/src/main/scala/com/bwsw/sj/engine/core/simulation/state/CommonEngineSimulator.scala
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.bwsw.sj.engine.core.simulation.state
+
+import com.bwsw.sj.common.engine.core.entities.{Envelope, KafkaEnvelope, TStreamEnvelope}
+import com.bwsw.sj.common.engine.{CheckpointHandlers, StateHandlers, StreamingExecutor}
+import com.bwsw.sj.engine.core.simulation.SimulatorConstants.defaultConsumerName
+
+import scala.collection.mutable
+
+/**
+  * Provides methods for [[com.bwsw.sj.engine.core.simulation.regular.RegularEngineSimulator]] and
+  * [[com.bwsw.sj.engine.core.simulation.batch.BatchEngineSimulator]]
+  *
+  * @author Pavel Tomskikh
+  */
+class CommonEngineSimulator[T <: AnyRef](executor: StreamingExecutor with StateHandlers with CheckpointHandlers,
+                                         manager: ModuleEnvironmentManagerMock) {
+
+  protected var transactionID: Long = 0
+  protected val inputEnvelopes: mutable.Buffer[Envelope] = mutable.Buffer.empty
+
+  /**
+    * Load state in state storage
+    *
+    * @param state key/value map
+    */
+  def prepareState(state: Map[String, Any]): Unit = state.foreach {
+    case (key, value) =>
+      manager.getState.set(key, value)
+  }
+
+  /**
+    * Creates [[TStreamEnvelope]] and saves it in a local buffer.
+    *
+    * @param entities     incoming data
+    * @param stream       name of stream
+    * @param consumerName name of consumer
+    * @return ID of saved transaction
+    */
+  def prepareTstream(entities: Seq[T], stream: String, consumerName: String = defaultConsumerName): Long = {
+    val queue = mutable.Queue(entities: _*)
+    val envelope = new TStreamEnvelope[T](queue, consumerName)
+    transactionID += 1
+    envelope.id = transactionID
+    envelope.stream = stream
+
+    inputEnvelopes += envelope
+    transactionID
+  }
+
+  /**
+    * Creates [[KafkaEnvelope]] and saves it in a local buffer
+    *
+    * @param entity incoming data
+    * @param stream name of stream
+    * @return ID of saved transaction
+    */
+  def prepareKafka(entity: T, stream: String): Long = {
+    val envelope = new KafkaEnvelope[T](entity)
+    transactionID += 1
+    envelope.id = transactionID
+    envelope.stream = stream
+
+    inputEnvelopes += envelope
+    transactionID
+  }
+
+  /**
+    * Creates [[KafkaEnvelope]]s and saves it in a local buffer. For each element from entities creates one
+    * [[KafkaEnvelope]].
+    *
+    * @param entities incoming data
+    * @param stream   name of stream
+    * @return IDs of saved transactions
+    */
+  def prepareKafka(entities: Seq[T], stream: String): Seq[Long] =
+    entities.map(prepareKafka(_, stream))
+
+  /**
+    * Simulates behavior of [[com.bwsw.sj.common.engine.TaskEngine TaskEngine]] before checkpoint
+    *
+    * @param isFullState flag denotes that the full state(true) or partial changes of state(false) is going to be saved
+    * @return output elements and state
+    */
+  def beforeCheckpoint(isFullState: Boolean): SimulationResult = {
+    executor.onBeforeCheckpoint()
+    executor.onBeforeStateSave(isFullState)
+
+    simulationResult
+  }
+
+  /**
+    * Simulates behavior of [[com.bwsw.sj.common.engine.TaskEngine TaskEngine]] after checkpoint
+    *
+    * @param isFullState flag denotes that there was saved the full state(true) or partial changes of state(false)
+    * @return output elements and state
+    */
+  def afterCheckpoint(isFullState: Boolean): SimulationResult = {
+    manager.producerPolicyByOutput.values.foreach {
+      case (_, moduleOutput: ModuleOutputMockHelper) =>
+        moduleOutput.clear()
+      case _ =>
+        throw new IllegalStateException("Incorrect outputs")
+    }
+
+    executor.onAfterCheckpoint()
+    executor.onAfterStateSave(isFullState)
+
+    simulationResult
+  }
+
+
+  protected def simulationResult = {
+    val streamData = manager.producerPolicyByOutput.map {
+      case (stream, (_, moduleOutput: ModuleOutputMockHelper)) =>
+        StreamData(stream, moduleOutput.getPartitionDataList)
+      case _ =>
+        throw new IllegalStateException("Incorrect outputs")
+    }.toSeq
+
+    SimulationResult(streamData, manager.getState.getAll)
+  }
+}
