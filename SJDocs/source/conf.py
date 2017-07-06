ccaeaea7084bc2d884f9e19286be65c2b6aa482d
MaslovaEV <maslova_ev@bw-sw.com>
2017-07-06 11:15:47 +0700

    Update conf.py

--- SJDocs/source/conf.py
+++ SJDocs/source/conf.py
@@ -31,7 +31,7 @@ import os
 # ones.
 extensions = [
     'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,7 +40,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst', '.md'
+source_suffix = '.rst'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
