16bc812ee7f48f5b38e1bc1f3917c8b45290db94
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-07-13 10:00:26 +0700

    SJ-3917 add possibility to choose where mongo will be write logs

--- core/sj-common/src/main/scala/com/bwsw/common/embedded/EmbeddedMongo.scala
+++ core/sj-common/src/main/scala/com/bwsw/common/embedded/EmbeddedMongo.scala
@@ -18,22 +18,42 @@
  */
 package com.bwsw.common.embedded
 
-import de.flapdoodle.embed.mongo.MongodStarter
-import de.flapdoodle.embed.mongo.config.{MongodConfigBuilder, Net}
+import de.flapdoodle.embed.mongo.config.{MongodConfigBuilder, Net, RuntimeConfigBuilder}
 import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.mongo.{Command, MongodStarter}
+import de.flapdoodle.embed.process.config.io.ProcessOutput
+import de.flapdoodle.embed.process.io.{Processors, Slf4jLevel}
+import org.slf4j.LoggerFactory
 
 /**
   * Used for testing purposes only. Mongo DB server.
   *
-  * @param port mongo port
+  * @param port           mongo port
+  * @param logIntoConsole flag denotes that logs will be writen into the console (true) or into a log-file (false)
   * @author Pavel Tomskikh
   */
-class EmbeddedMongo(port: Int) {
+class EmbeddedMongo(port: Int, logIntoConsole: Boolean = false) {
   private val config = new MongodConfigBuilder().net(new Net(port, false)).version(Version.V3_5_1).build()
-  private val mongodStarter = MongodStarter.getDefaultInstance
+  private val mongodStarter = createMongodStarter()
   private val mongo = mongodStarter.prepare(config)
 
   def start(): Unit = mongo.start()
 
   def stop(): Unit = mongo.stop()
+
+
+  private def createMongodStarter(): MongodStarter = {
+    if (logIntoConsole)
+      MongodStarter.getDefaultInstance
+    else {
+      val logger = LoggerFactory.getLogger(getClass)
+      val processOutput = new ProcessOutput(
+        Processors.logTo(logger, Slf4jLevel.INFO),
+        Processors.logTo(logger, Slf4jLevel.INFO),
+        Processors.logTo(logger, Slf4jLevel.INFO))
+      val runtimeConfig = new RuntimeConfigBuilder().defaults(Command.MongoD).processOutput(processOutput).build()
+
+      MongodStarter.getInstance(runtimeConfig)
+    }
+  }
 }
