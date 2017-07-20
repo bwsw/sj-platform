c1ea7ff6be124ff59d1bdf1f4d0250c9f98302e1
ksenya bauer <bauer_ks@bw-sw.com>
2017-07-20 08:56:39 +0700

    SJ-3950 - after testing

--- core/sj-frontend/src/app/shared/pipes/service-filter.pipe.ts
+++ core/sj-frontend/src/app/shared/pipes/service-filter.pipe.ts
@@ -9,8 +9,8 @@ export class ServiceFilterPipe implements PipeTransform {
     const stream_types = ['stream.apache-kafka', 'stream.t-streams', 'stream.elasticsearch', 'stream.sql-database', 'stream.restful'];
     const service_types = ['service.apache-kafka', 'service.t-streams', 'service.elasticsearch', 'service.sql-database', 'service.restful'];
     if (term !== undefined) {
-      if (term === 'zookeeper') {
-        return value.filter((entity) => entity.type.indexOf('ZKCoord') > -1);
+      if (term === 'service.apache-zookeeper') {
+        return value.filter((entity) => entity.type.indexOf('service.apache-zookeeper') > -1);
       } else {
         const index = stream_types.indexOf(term);
         const term_type = service_types[index];
