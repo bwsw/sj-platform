3347bad96fc8de16975b6a37fd5b409f7df86fff
mikhaljova_ka <buterffly@sibmail.com>
2017-08-10 10:14:44 +0700

    SJ-4011 fix NPE by adding a test of non-empty 'options' field and the appropriate error message

--- contrib/sj-platform/sj-regex-input/src/main/scala/com/bwsw/sj/module/input/regex/RegexInputValidator.scala
+++ contrib/sj-platform/sj-regex-input/src/main/scala/com/bwsw/sj/module/input/regex/RegexInputValidator.scala
@@ -56,25 +56,26 @@ class RegexInputValidator extends StreamingValidator {
     val serializer = new JsonSerializer
     val regexInputOptions = serializer.deserialize[RegexInputOptions](options)
 
-    if (!isRequiredStringField(Option(regexInputOptions.lineSeparator)))
-      errors += s"'${RegexInputOptionsNames.lineSeparator}' attribute is required and should be a non-empty string"
-    if (!isRequiredStringField(Option(regexInputOptions.policy)))
-      errors += s"'${RegexInputOptionsNames.policy}' attribute is required and should be a non-empty string"
-
-    if (!isRequiredStringField(Option(regexInputOptions.encoding)))
-      errors += s"'${RegexInputOptionsNames.encoding}' attribute is required and should be a non-empty string"
-    else {
-      if (!Charset.isSupported(regexInputOptions.encoding))
-        errors += s"'${RegexInputOptionsNames.encoding}' is not supported"
+    if (!regexInputOptions.isEmpty()) {
+      if (!isRequiredStringField(Option(regexInputOptions.lineSeparator)))
+        errors += s"'${RegexInputOptionsNames.lineSeparator}' attribute is required and should be a non-empty string"
+      if (!isRequiredStringField(Option(regexInputOptions.policy)))
+        errors += s"'${RegexInputOptionsNames.policy}' attribute is required and should be a non-empty string"
+      if (!isRequiredStringField(Option(regexInputOptions.encoding)))
+        errors += s"'${RegexInputOptionsNames.encoding}' attribute is required and should be a non-empty string"
+      else {
+        if (!Charset.isSupported(regexInputOptions.encoding))
+          errors += s"'${RegexInputOptionsNames.encoding}' is not supported"
+      }
+      if (!isRequiredStringField(Option(regexInputOptions.fallbackStream)))
+        errors += s"'${RegexInputOptionsNames.fallbackStream}' attribute is required and should be a non-empty string"
+      if (regexInputOptions.rules.isEmpty)
+        errors += s"'${RegexInputOptionsNames.rules}' attribute is required and should be a non-empty set"
+      if (!regexInputOptions.rules.forall(validateRule))
+        errors += s"'${RegexInputOptionsNames.rules}' hasn't passed validation"
+    } else {
+      errors += s"'options' attribute is required"
     }
-
-    if (!isRequiredStringField(Option(regexInputOptions.fallbackStream)))
-      errors += s"'${RegexInputOptionsNames.fallbackStream}' attribute is required and should be a non-empty string"
-
-    if (regexInputOptions.rules.isEmpty)
-      errors += s"'${RegexInputOptionsNames.rules}' attribute is required and should be a non-empty set"
-    if (!regexInputOptions.rules.forall(validateRule))
-      errors += s"'${RegexInputOptionsNames.rules}' hasn't passed validation"
 
     ValidationInfo(errors.isEmpty, errors)
   }
