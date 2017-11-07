15c1c47cf1d2a29d23987ae7de5ebfe553684f04
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2017-11-07 17:35:53 +0700

    SJ-4184 add principal in framework info (FrameworkStarter)

--- core/sj-mesos-framework/src/main/scala/com/bwsw/sj/mesos/framework/FrameworkStarter.scala
+++ core/sj-mesos-framework/src/main/scala/com/bwsw/sj/mesos/framework/FrameworkStarter.scala
@@ -18,17 +18,13 @@
  */
 package com.bwsw.sj.mesos.framework
 
-import java.lang.Enum
-
 import com.bwsw.common.LeaderLatch
 import com.bwsw.sj.common.config.ConfigLiterals
-import com.bwsw.sj.common.dal.model.ConfigurationSettingDomain
 import com.bwsw.sj.common.dal.repository.ConnectionRepository
 import com.bwsw.sj.common.utils.FrameworkLiterals
 import com.bwsw.sj.mesos.framework.config.FrameworkConfigNames
 import com.bwsw.sj.mesos.framework.rest.Rest
 import com.bwsw.sj.mesos.framework.schedule.{FrameworkScheduler, FrameworkUtil}
-import com.google.protobuf.Enum
 import com.typesafe.config.ConfigFactory
 import org.apache.mesos.MesosSchedulerDriver
 import org.apache.mesos.Protos.{Credential, FrameworkInfo, Status}
@@ -43,18 +39,18 @@ object FrameworkStarter {
 
   private val config = ConfigFactory.load()
   FrameworkUtil.config = Option(config)
-  val frameworkName = "JugglerFramework"
-  val frameworkUser = Try(config.getString(FrameworkConfigNames.user)).getOrElse("root")
-  val frameworkCheckpoint = false
-  val frameworkFailoverTimeout = 0.0d
-  val frameworkRole = "*"
+  private val frameworkName = "JugglerFramework"
+  private val frameworkUser = Try(config.getString(FrameworkConfigNames.user)).getOrElse("root")
+  private val frameworkCheckpoint = false
+  private val frameworkFailoverTimeout = 0.0d
+  private val frameworkRole = "*"
 
-  val master_path = Try(config.getString(FrameworkLiterals.mesosMaster)).getOrElse("zk://127.0.0.1:2181/mesos")
-  val zookeeper_host = Try(config.getString(FrameworkLiterals.zookeeperHost)).getOrElse("127.0.0.1")
-  val zookeeper_port = Try(config.getString(FrameworkLiterals.zookeeperPort)).getOrElse("2181")
-  val frameworkTaskId = Try(config.getString(FrameworkLiterals.frameworkId)).getOrElse("broken")
+  private val master_path = Try(config.getString(FrameworkLiterals.mesosMaster)).getOrElse("zk://127.0.0.1:2181/mesos")
+  private val zookeeper_host = Try(config.getString(FrameworkLiterals.zookeeperHost)).getOrElse("127.0.0.1")
+  private val zookeeper_port = Try(config.getString(FrameworkLiterals.zookeeperPort)).getOrElse("2181")
+  private val frameworkTaskId = Try(config.getString(FrameworkLiterals.frameworkId)).getOrElse("broken")
 
-  val connectionRepository: ConnectionRepository = inject[ConnectionRepository]
+  private val connectionRepository: ConnectionRepository = inject[ConnectionRepository]
 
   /**
     * Main function to start rest and framework.
@@ -66,19 +62,20 @@ object FrameworkStarter {
     Rest.start(port)
     FrameworkUtil.instancePort = Option(port)
 
+    val frameworkPrincipal = connectionRepository.getConfigRepository.get(ConfigLiterals.frameworkPrincipalTag)
+    val frameworkSecret = connectionRepository.getConfigRepository.get(ConfigLiterals.frameworkSecretTag)
+
     val frameworkInfo = FrameworkInfo.newBuilder.
       setName(frameworkName).
       setUser(frameworkUser).
       setCheckpoint(frameworkCheckpoint).
       setFailoverTimeout(frameworkFailoverTimeout).
       setRole(frameworkRole).
-      setPrincipal("sherman").
+      setPrincipal(frameworkPrincipal.map(_.value).getOrElse("sherman")).
       build()
 
     val scheduler = new FrameworkScheduler
 
-    val frameworkPrincipal = connectionRepository.getConfigRepository.get(ConfigLiterals.frameworkPrincipalTag)
-    val frameworkSecret = connectionRepository.getConfigRepository.get(ConfigLiterals.frameworkSecretTag)
     var credential: Option[Credential] = None
 
     if (frameworkPrincipal.isDefined && frameworkSecret.isDefined) {
